90ea212a848dcc465eb19d03f3c9f0efc3b05c5f
Ian Ibbotson <ianibbo@googlemail.com>
2019-01-29 15:28:13 +0000

    Working though linter output

--- src/components/patron-request/edit-patron-request.js
+++ src/components/patron-request/edit-patron-request.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { cloneDeep, get } from 'lodash';
-import { FormattedMessage } from 'react-intl';
-
-import {
-  AccordionSet,
-  Button,
-  Col,
-  ExpandAllButton,
-  Icon,
-  Layer,
-  Layout,
-  Pane,
-  Row,
-} from '@folio/stripes/components';
 
 class EditPatronRequest extends React.Component {
   render() {
-    return ( <h1>Hello</h1> );
+    return (<h1>Hello</h1>);
   }
 }
 
