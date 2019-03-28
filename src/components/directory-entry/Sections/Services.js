32e5771b27886450ea7ed39d3f5493af74c70fdc
Mike Taylor <mike@indexdata.com>
2019-03-28 12:28:19 +0000

    New file, Services.js

---
+++ src/components/directory-entry/Sections/Services.js
@@ -0,0 +1,68 @@
+import _ from 'lodash';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Accordion, KeyValue, Row, Col } from '@folio/stripes/components';
+
+
+function renderService(service, index, count) {
+  // XXX I don't know what service.accountDetails is
+  const ss = service.service;
+  const header = ss.name ||
+    <FormattedMessage id="ui-directory.information.serviceNofM" values={{ index, count }} />;
+
+  return (
+    <React.Fragment key={index}>
+      <h2>{header}</h2>
+      <Row>
+        <Col xs={6}>
+          <KeyValue
+            label={<FormattedMessage id="ui-directory.information.serviceFunction" />}
+            value={ss.businessFunction.value}
+          />
+        </Col>
+        <Col xs={6}>
+          <KeyValue
+            label={<FormattedMessage id="ui-directory.information.serviceType" />}
+            value={ss.type.value}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <KeyValue
+            label={<FormattedMessage id="ui-directory.information.serviceAddress" />}
+            value={<tt>{ss.address}</tt>}
+          />
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
+}
+
+
+class Services extends React.Component {
+  static propTypes = {
+    directoryEntry: PropTypes.object,
+    id: PropTypes.string,
+    onToggle: PropTypes.func,
+    open: PropTypes.bool,
+  };
+
+  render() {
+    const services = this.props.directoryEntry.services || [];
+
+    return (
+      <Accordion
+        id={this.props.id}
+        label={<FormattedMessage id="ui-directory.information.heading.services" />}
+        open={this.props.open}
+        onToggle={this.props.onToggle}
+      >
+        {services.map((service, i) => renderService(service, i + 1, services.length))}
+      </Accordion>
+    );
+  }
+}
+
+export default Services;
