a1fc4fce2b49931476f72bcbe9ffa99ad2867bdd
Mike Taylor <mike@indexdata.com>
2019-03-28 12:28:32 +0000

    Export Services component

--- src/components/directory-entry/Sections/index.js
+++ src/components/directory-entry/Sections/index.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export { default as DirectoryEntryInfo } from './DirectoryEntryInfo';
 export { default as Addresses } from './Addresses';
+export { default as Services } from './Services';
 export { default as CustomProperties } from './CustomProperties';
