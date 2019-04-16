b6ee8fdbf008a4ea7a0193d39b3d7fbefe197e01
Mike Taylor <mike@indexdata.com>
2019-04-16 17:15:13 +0100

    Towards support for editing

--- src/components/directory-entry/edit-directory-entry.js
+++ src/components/directory-entry/edit-directory-entry.js
@@ -1,9 +1,106 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import stripesForm from '@folio/stripes/form';
+
+import {
+  Button,
+  IconButton,
+  Pane,
+  PaneMenu,
+} from '@folio/stripes/components';
+
+import DirEntryForm from './DirEntryForm';
+
+const handleSubmit = (agreement, dispatch, props) => {
+  props.onUpdate(agreement)
+    .then(() => props.onCancel());
+};
 
 class EditDirectoryEntry extends React.Component {
+  static propTypes = {
+    initialValues: PropTypes.object,
+    handleSubmit: PropTypes.func.isRequired,
+    // onSave: PropTypes.func,
+    onCancel: PropTypes.func,
+    // onRemove: PropTypes.func,
+    onUpdate: PropTypes.func.isRequired,
+    pristine: PropTypes.bool,
+    submitting: PropTypes.bool,
+    // parentResources: PropTypes.object,
+    // parentMutator: PropTypes.object,
+  }
+
+  renderFirstMenu() {
+    return (
+      <PaneMenu>
+        <FormattedMessage id="ui-directory.closeNewDirEntry">
+          {ariaLabel => (
+            <IconButton
+              icon="times"
+              id="close-directory-form-button"
+              onClick={this.props.onCancel}
+              aria-label={ariaLabel}
+            />
+          )}
+        </FormattedMessage>
+      </PaneMenu>
+    );
+  }
+
+  renderLastMenu() {
+    const { initialValues } = this.props;
+
+    let id;
+    let label;
+    if (initialValues && initialValues.id) {
+      id = 'clickable-update-directory-entry';
+      label = <FormattedMessage id="ui-directory.updateDirEntry" />;
+    } else {
+      id = 'clickable-create-directory-entry';
+      label = <FormattedMessage id="ui-directory.createDirEntry" />;
+    }
+
+    return (
+      <PaneMenu>
+        <Button
+          id={id}
+          type="submit"
+          disabled={this.props.pristine || this.props.submitting}
+          onClick={this.props.handleSubmit}
+          buttonStyle="primary paneHeaderNewButton"
+          marginBottom0
+        >
+          {label}
+        </Button>
+      </PaneMenu>
+    );
+  }
+
   render() {
-    return (<h1>Hello</h1>);
+    const { initialValues } = this.props;
+    const paneTitle = initialValues && initialValues.id ?
+      initialValues.name : <FormattedMessage id="ui-directory.createDirEntry" />;
+
+    return (
+      <form id="form-directory-entry">
+        <Pane
+          defaultWidth="100%"
+          firstMenu={this.renderFirstMenu()}
+          lastMenu={this.renderLastMenu()}
+          paneTitle={paneTitle}
+        >
+          <DirEntryForm {...this.props} />
+        </Pane>
+      </form>
+    );
   }
 }
 
-export default EditDirectoryEntry;
+export default stripesForm({
+  form: 'directoryForm',
+  onSubmit: handleSubmit,
+  navigationCheck: true,
+  enableReinitialize: true,
+  keepDirtyOnReinitialize: true,
+})(EditDirectoryEntry);
