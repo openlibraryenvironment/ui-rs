f85be3111e4a66f9e6f6235c313c02ab58bb2034
Mike Taylor <mike@indexdata.com>
2019-04-16 15:36:07 +0100

    New, placeholder

---
+++ src/components/directory-entry/DirEntryForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class DirEntryForm extends React.Component {
+  render() {
+    return (
+      <p>Directory-entry form</p>
+    );
+  }
+}
+
+export default DirEntryForm;
