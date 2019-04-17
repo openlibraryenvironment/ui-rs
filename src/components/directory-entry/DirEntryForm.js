f5640eadacc539d0858405a6a603dd6e954a0dd9
Mike Taylor <mike@indexdata.com>
2019-04-17 17:20:59 +0100

    Towards a functional form

--- src/components/directory-entry/DirEntryForm.js
+++ src/components/directory-entry/DirEntryForm.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  AccordionSet,
+  Col,
+  ExpandAllButton,
+  Row
+} from '@folio/stripes/components';
+
+/*
+import {
+  DirEntryFormInfo,
+} from './sections';
+*/
+
+function DirEntryFormInfo() { return <p>form info</p>; }
 
 class DirEntryForm extends React.Component {
+  static propTypes = {
+    // XXX I don't think we need either of these
+    parentResources: PropTypes.object,
+    parentMutator: PropTypes.object,
+  }
+
+  state = {
+    sections: {
+      dirEntryFormInfo: true,
+    }
+  }
+
+  getSectionProps() {
+    return {
+      onToggle: this.handleSectionToggle,
+      parentResources: this.props.parentResources,
+      parentMutator: this.props.parentMutator,
+    };
+  }
+
+  handleSectionToggle = ({ id }) => {
+    this.setState((prevState) => ({
+      sections: {
+        ...prevState.sections,
+        [id]: !prevState.sections[id],
+      }
+    }));
+  }
+
+  handleAllSectionsToggle = (sections) => {
+    this.setState({ sections });
+  }
+
   render() {
+    const sectionProps = this.getSectionProps();
+    const { sections } = this.state;
+
     return (
-      <p>Directory-entry form</p>
+      <div>
+        <AccordionSet>
+          <Row end="xs">
+            <Col xs>
+              <ExpandAllButton
+                accordionStatus={sections}
+                onToggle={this.handleAllSectionsToggle}
+              />
+            </Col>
+          </Row>
+          <DirEntryFormInfo id="dirEntryFormInfo" open={sections.dirEntryFormInfo} {...sectionProps} />
+        </AccordionSet>
+      </div>
     );
   }
 }
