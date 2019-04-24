7b98d5c87920a49cee10d479caf1b5d170aee478
Mike Taylor <mike@indexdata.com>
2019-04-24 18:08:21 +0100

    Add code for editing a tiny part of the record

--- src/components/DirectoryEntryForm/sections/DirectoryEntryFormInfo.js
+++ src/components/DirectoryEntryForm/sections/DirectoryEntryFormInfo.js
@@ -1,3 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { get } from 'lodash';
+import { Field } from 'redux-form';
 
-export default function DirectoryEntryFormInfo() { return <p>form info in its own file</p>; }
+import {
+  Accordion,
+  Col,
+  Row,
+  TextField,
+} from '@folio/stripes/components';
+
+import { required } from '../../../util/validators';
+
+class DirectoryEntryFormInfo extends React.Component {
+  static propTypes = {
+    id: PropTypes.string,
+    onToggle: PropTypes.func,
+    open: PropTypes.bool,
+    parentResources: PropTypes.shape({
+      typeValues: PropTypes.object,
+    }),
+  };
+
+  getTypeValues() {
+    return get(this.props.parentResources.typeValues, ['records'], [])
+      .map(({ id, label }) => ({ label, value: id }));
+  }
+
+  render() {
+    return (
+      <Accordion
+        id={this.props.id}
+        label={<FormattedMessage id="ui-directory.information.heading.directoryEntry" />}
+        open={this.props.open}
+        onToggle={this.props.onToggle}
+      >
+        <React.Fragment>
+          <Row>
+            <Col xs={5}>
+              <FormattedMessage id="ui-directory.information.name">
+                {placeholder => (
+                  <Field
+                    id="edit-directory-entry-name"
+                    name="name"
+                    label="LABEL"
+                    component={TextField}
+                    placeholder={placeholder}
+                    required
+                    validate={required}
+                  />
+                )}
+              </FormattedMessage>
+            </Col>
+            <Col xs={2}>
+              status
+            </Col>
+            <Col xs={5}>
+              slug
+            </Col>
+          </Row>
+        </React.Fragment>
+      </Accordion>
+    );
+  }
+}
+
+export default DirectoryEntryFormInfo;
