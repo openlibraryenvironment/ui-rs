9f24f0159d045b7821f4a28bd91a7023b8b3c52e
Mike Taylor <mike@indexdata.com>
2019-06-10 17:50:27 +0100

    Towards compatibility with directory module

--- src/components/PatronRequest/ViewPatronRequest.js
+++ src/components/PatronRequest/ViewPatronRequest.js
@@ -1,5 +1,6 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 import { get } from 'lodash';
 
 import {
@@ -11,11 +12,13 @@ import {
   Button,
 } from '@folio/stripes/components';
 
+import EditPatronRequest from '../EditPatronRequest';
+
 import PatronRequestInfo from './Sections/PatronRequestInfo';
 
 class ViewPatronRequest extends React.Component {
   static manifest = Object.freeze({
-    selectedPatronRequest: {
+    selectedRecord: {
       type: 'okapi',
       path: 'rs/patronrequests/:{id}',
     },
@@ -28,18 +31,16 @@ class ViewPatronRequest extends React.Component {
       query: PropTypes.shape({
         layer: PropTypes.string,
       }),
-      selectedPatronRequest: PropTypes.shape({
+      selectedRecord: PropTypes.shape({
         records: PropTypes.array,
       }),
     }),
     paneWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     onClose: PropTypes.func,
+    onEdit: PropTypes.func,
+    editLink: PropTypes.string,
+    onCloseEdit: PropTypes.func,
   };
-
-  constructor(props) {
-    super(props);
-    this.getActionMenu = this.getActionMenu.bind(this);
-  }
 
   state = {
     sections: {
@@ -47,13 +48,18 @@ class ViewPatronRequest extends React.Component {
     }
   }
 
-  getPatronRequest() {
-    return get(this.props.resources.selectedPatronRequest, ['records', 0], {});
+  getRecord() {
+    return get(this.props.resources.selectedRecord, ['records', 0], {});
+  }
+
+  getInitialValues = () => {
+    const record = Object.assign({}, this.getRecord());
+    return record;
   }
 
   getSectionProps() {
     return {
-      patronRequest: this.getPatronRequest(),
+      patronRequest: this.getRecord(),
       onToggle: this.handleSectionToggle,
       stripes: this.props.stripes,
     };
@@ -71,62 +77,53 @@ class ViewPatronRequest extends React.Component {
   renderEditLayer() {
     const { resources: { query } } = this.props;
 
-    return ( // XXX do this bit first
-      <Layer
-        isOpen={query.layer === 'edit'}
-        contentLabel="cant get intl to work"
-      >
-        Editing ...
-      </Layer>
+    return (
+      <FormattedMessage id="ui-rs.editPatronRequest">
+        {layerContentLabel => (
+          <Layer
+            isOpen={query.layer === 'edit'}
+            contentLabel={layerContentLabel}
+          >
+            <EditPatronRequest
+              {...this.props}
+              onCancel={this.props.onCloseEdit}
+              onSubmit={this.handleSubmit}
+              initialValues={this.getInitialValues()}
+            />
+          </Layer>
+        )}
+      </FormattedMessage>
     );
   }
 
-  getActionMenu({ onToggle }) {
-    const items = [];
-    /**
-     * We only want to render the action menu
-     * if we have something to show
-     */
-    if (!items.length) {
-      return null;
-    }
-
-    /**
-     * Return action menu
-     */
+  getActionMenu = ({ onToggle }) => {
     return (
-      <Fragment>
-        {items.map((item, index) => (
-          <Button
-            key={index}
-            buttonStyle="dropdownItem"
-            id={item.id}
-            aria-label={item.ariaLabel}
-            href={item.href}
-            onClick={() => {
-              // Toggle the action menu dropdown
-              onToggle();
-              item.onClick();
-            }}
-          >
-            <Icon icon={item.icon}>
-              {item.label}
-            </Icon>
-          </Button>
-        ))}
-      </Fragment>
+      <Button
+        buttonStyle="dropdownItem"
+        href={this.props.editLink}
+        id="clickable-edit-patronrequest"
+        onClick={() => {
+          this.props.onEdit();
+          onToggle();
+        }}
+      >
+        <Icon icon="edit">
+          <FormattedMessage id="ui-rs.edit" />
+        </Icon>
+      </Button>
     );
   }
 
   render() {
-    const patronRequest = this.getPatronRequest();
+    const record = this.getRecord();
     const sectionProps = this.getSectionProps();
+    const title = record.id;
 
     return (
       <Pane
-        id="pane-view-patron-request"
+        id="pane-view-patronrequest"
         defaultWidth={this.props.paneWidth}
-        paneTitle={patronRequest.id}
+        paneTitle={title}
         dismissible
         onClose={this.props.onClose}
         actionMenu={this.getActionMenu}
@@ -139,7 +136,7 @@ class ViewPatronRequest extends React.Component {
             displayWhenClosed="test2"
             {...sectionProps}
           >
-            <pre>{JSON.stringify(patronRequest, null, 2)}</pre>
+            <pre>{JSON.stringify(record, null, 2)}</pre>
           </Accordion>
         </AccordionSet>
         { this.renderEditLayer() }
