d7e4b868cba37ccb4139b900decb41929eef5d32
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2019-10-15 16:29:10 +0100

    PR for Settings Pane to hold Z3950 address (#4)
    * Got a settings pane containing addresses in the system
    
    * Cleaned up translation tag names for PR
    
    * Removed capitalisation of translations, patched a gap in translations

---
+++ src/settings/SupplyAddressSettings.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStripes } from '@folio/stripes/core';
+import { Pane } from '@folio/stripes/components';
+import { ControlledVocab } from '@folio/stripes/smart-components';
+import { FormattedMessage } from 'react-intl';
+
+class SupplyAddressSettings extends React.Component {
+  static manifest = Object.freeze({
+    setting: {
+      type: 'okapi',
+      path: 'rs/settings/appSettings?stats=true',
+    },
+  });
+
+  static propTypes = {
+    label: PropTypes.string.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.connectedControlledVocab = props.stripes.connect(ControlledVocab);
+  }
+  render() {
+    const{ stripes } = this.props;
+    return (
+      <this.connectedControlledVocab
+        stripes={stripes}
+        baseUrl="rs/settings/appSettings"
+        label={<FormattedMessage id="ui-rs.settings.supply-address.addresses" />}
+        labelSingular={<FormattedMessage id="ui-rs.settings.supply-address.address" />}
+        objectLabel={<FormattedMessage id="ui-rs.settings.supply-address.addresses" />}
+        visibleFields={['key', 'value']}
+        columnMapping={{
+          st_key: <FormattedMessage id="ui-rs.settings.supply-address.name" />,
+          st_value: <FormattedMessage id="ui-rs.settings.supply-address.value" />,
+        }}
+        id="addresses"
+        sortby="value"
+        hiddenFields={['id', 'version', 'section', 'settingType', 'vocab', 'defValue', 'lastUpdated','numberOfObjects']}
+        limitParam="perPage"
+      />
+    );
+  }
+}
+
+export default withStripes(SupplyAddressSettings);
