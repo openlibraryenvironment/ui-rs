910ff173754d61f2578fce8742268351f13eb022
Mike Taylor <mike@indexdata.com>
2019-10-18 12:53:25 +0100

    First attempt at using ReactToPrint

--- src/components/PrintPullSlip.js
+++ src/components/PrintPullSlip.js
@@ -1,29 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import ReactToPrint from 'react-to-print';
 import { AccordionSet, Accordion } from '@folio/stripes/components';
 import PullSlip from './PullSlip';
 
-const PrintPullSlip = (props) => {
-  return (
-    <div>
-      <PullSlip record={props.record} />
-      <AccordionSet>
-        <Accordion
-          id="callslip-developerInfo"
-          closedByDefault
-          label={<FormattedMessage id="ui-rs.information.heading.developer" />}
-          displayWhenClosed={<FormattedMessage id="ui-rs.information.heading.developer.help" />}
-        >
-          <pre>{JSON.stringify(props.record, null, 2)}</pre>
-        </Accordion>
-      </AccordionSet>
-    </div>
-  );
-};
+class PrintPullSlip extends React.Component {
+  static propTypes = {
+    record: PropTypes.object.isRequired,
+  };
 
-PrintPullSlip.propTypes = {
-  record: PropTypes.object.isRequired,
-};
+  constructor(props) {
+    super(props);
+    this.editorRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <div>
+        <ReactToPrint
+          trigger={() => <button type="button">Print!</button>}
+          content={() => this.editorRef.current}
+        />
+        <div ref={this.editorRef}>
+          <PullSlip record={this.props.record} />
+        </div>
+
+        <AccordionSet>
+          <Accordion
+            id="callslip-developerInfo"
+            closedByDefault
+            label={<FormattedMessage id="ui-rs.information.heading.developer" />}
+            displayWhenClosed={<FormattedMessage id="ui-rs.information.heading.developer.help" />}
+          >
+            <pre>{JSON.stringify(this.props.record, null, 2)}</pre>
+          </Accordion>
+        </AccordionSet>
+      </div>
+    );
+  }
+}
 
 export default PrintPullSlip;
