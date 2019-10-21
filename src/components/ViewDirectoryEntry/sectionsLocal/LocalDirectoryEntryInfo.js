9bd097cac8fd262dd40c6030f0f4f76889cc3da4
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-10-21 16:25:49 +0100

    Added the files relevant to the previous commit

---
+++ src/components/ViewDirectoryEntry/sectionsLocal/LocalDirectoryEntryInfo.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import {
+  Accordion,
+  Col,
+  KeyValue,
+  Row,
+} from '@folio/stripes/components';
+
+class LocalDirectoryEntryInfo extends React.Component {
+  static propTypes = {
+    record: PropTypes.object,
+    id: PropTypes.string,
+    onToggle: PropTypes.func,
+    open: PropTypes.bool,
+  };
+
+  render() {
+    const { record } = this.props;
+
+    return (
+      <Accordion
+        id={this.props.id}
+        label={<FormattedMessage id="ui-directory.information.local.heading.directoryEntry" />}
+        open={this.props.open}
+        onToggle={this.props.onToggle}
+      >
+        <Row>
+          <Col xs={6}>
+            <KeyValue
+              label={<FormattedMessage id="ui-directory.information.local.patronAccountBarcode" />}
+              value={record.customProperties.local_patronAccountBarcode ? record.customProperties.local_patronAccountBarcode : '-'}
+            />
+          </Col>
+        </Row>
+      </Accordion>
+    );
+  }
+}
+
+export default LocalDirectoryEntryInfo;
