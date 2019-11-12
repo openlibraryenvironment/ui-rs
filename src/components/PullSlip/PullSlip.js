7466960d5db364cace2f0f0bded1fe58cf228b81
Mike Taylor <mike@indexdata.com>
2019-11-12 18:21:10 +0000

    Furnish those fields I can find in a sample record

--- src/components/PullSlip/PullSlip.js
+++ src/components/PullSlip/PullSlip.js
@@ -1,9 +1,32 @@
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Parser } from 'html-to-react';
+import get from 'lodash/get';
 import template from './design/pullslip.handlebars';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import style from '!!style-loader?injectType=lazyStyleTag!postcss-loader!./design/style.css';
+
+function recordToData(record) {
+  return {
+    borrower: record.patronReference, // XXX Should be "Last Name, First Name"
+    pickupLocation: record.pickShelvingLocation,
+    requestBarcode: 'YYY requestBarcode',
+    requestId: record.id,
+    title: record.title,
+    author: record.author,
+    volume: 'XXX volume',
+    phoneNumber: 'XXX phoneNumber',
+    emailAddress: 'XXX emailAddress',
+    callNumber: record.localCallNumber,
+    location: get(record, 'pickLocation.name'),
+    fromSlug: 'XXX fromSlug',
+    toSlug: record.requestingInstitutionSymbol, // XXX Should be slug from directory entry
+    now: new Date().toLocaleString(),
+    logo: 'ZZZ logo',
+    itemBarcode: 'YYY itemBarcode',
+    itemId: 'XXX itemId',
+  };
+}
 
 const PullSlip = (props) => {
   useEffect(() => {
@@ -13,7 +36,8 @@ const PullSlip = (props) => {
     };
   }, []);
 
-  const s = template(props.record);
+  const data = recordToData(props.record);
+  const s = template(data);
   const htmlToReactParser = new Parser();
   return htmlToReactParser.parse(s);
 };
