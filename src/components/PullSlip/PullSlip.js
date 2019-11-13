e15d0a1c32ddc076acb7e9722562a37a176f06e7
Mike Taylor <mike@indexdata.com>
2019-11-13 01:25:29 +0000

    First implementation of barcodes in template

--- src/components/PullSlip/PullSlip.js
+++ src/components/PullSlip/PullSlip.js
@@ -3,10 +3,35 @@ import PropTypes from 'prop-types';
 import { Parser } from 'html-to-react';
 import get from 'lodash/get';
 import { injectIntl } from 'react-intl';
+import JsBarcode from 'jsbarcode';
 import template from './design/pullslip.handlebars';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import style from '!!style-loader?injectType=lazyStyleTag!postcss-loader!./design/style.css';
 import logoUrl from './design/images/palci-logo.png';
+
+// Based on sample code in the "With svg" section at https://github.com/lindell/JsBarcode/blob/master/README.md
+function barCodeString(text, options) {
+  const { DOMImplementation, XMLSerializer } = require('xmldom');
+  const xmlSerializer = new XMLSerializer();
+  const document = new DOMImplementation().createDocument('http://www.w3.org/1999/xhtml', 'html', null);
+  const svgNode = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+  JsBarcode(svgNode, text, Object.assign({}, { xmlDocument: document }, options));
+  return xmlSerializer.serializeToString(svgNode);
+}
+
+function styledBarCodeString(text) {
+  return barCodeString(text, {
+    format: 'code39',
+    displayValue: false,
+    background: 'transparent',
+    lineColor: '#000000',
+    margin: 0,
+    marginLeft: 0,
+    height: 30,
+    width: 1
+  });
+}
 
 function recordToData(intl, record) {
   const now = new Date();
@@ -14,7 +39,7 @@ function recordToData(intl, record) {
   return {
     borrower: record.patronReference, // XXX Should be "Last Name, First Name"
     pickupLocation: record.pickShelvingLocation,
-    requestBarcode: 'YYY requestBarcode',
+    requestBarcode: styledBarCodeString(record.id.substring(0, 18)),
     requestId: record.id,
     title: record.title,
     author: record.author,
