4b3c8f3d7baea32578cc6392992c7eded7e8ee5c
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-11-19 13:06:58 +0000

    Deleted unnecessary component

--- src/settings/index.js
+++ src/settings/index.js
@@ -3,9 +3,41 @@ import { FormattedMessage } from 'react-intl';
 import { Settings } from '@folio/stripes/smart-components';
 import { stripesConnect } from '@folio/stripes/core';
 import SettingPage from './SettingPage';
-import SettingsPages from './SettingsPages';
 
 class ResourceSharingSettings extends React.Component { 
+  static manifest = Object.freeze({
+    settings: {
+      type: 'okapi',
+      path: 'rs/settings/appSettings',
+      //records: 'results',
+    },
+  });
+
+  getSectionsList() {
+    const rows = (this.props.resources.settings ? this.props.resources.settings.records : []);
+    const sections = Array.from(new Set(rows.map(obj => obj.section)))
+    return(sections);
+  }
+
+  customComponentMaker(section_name) {
+    const custComp = <SettingPage section_name={section_name} />
+    return (custComp);
+  }
+
+  pageList() {
+    let sections = this.getSectionsList();
+    const pages = sections.map(section => {
+      return (
+        {
+          "route": section.replace(' ', '_').toLowerCase(),
+          "label": section,
+          "component": this.customComponentMaker(section)
+        }
+      );
+    });
+    return ( pages );
+  }
+
   settingsSections = [
     {
       label: 'General',
@@ -30,11 +62,9 @@ class ResourceSharingSettings extends React.Component {
   ];
 
   render() {
+    console.log("Pages: %o", this.pageList())
     return (
-      <React.Fragment>
-        <SettingsPages />
       <Settings {...this.props} sections={this.settingsSections} paneTitle="Resource Sharing" />
-      </React.Fragment>
     );
   }
 }
