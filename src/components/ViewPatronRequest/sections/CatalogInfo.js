5f9f4c570b151359fd86eec76f476ad2d7247bee
Mike Taylor <mike@indexdata.com>
2019-12-09 18:36:21 +0000

    Include catalog record with link into SI
    Fixes much of PR-229, but several issues remain:
    * We are not yet able to find ISSN and ISBN in the record
    * The actual record that's embedded seems to always be the same one!

--- src/components/ViewPatronRequest/sections/CatalogInfo.js
+++ src/components/ViewPatronRequest/sections/CatalogInfo.js
@@ -4,12 +4,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import get from 'lodash/get';
 import { withStripes } from '@folio/stripes/core';
 import {
   Accordion,
   Card,
   Col,
   Row,
+  KeyValue,
 } from '@folio/stripes/components';
 
 import css from './CatalogInfo.css';
@@ -29,6 +31,29 @@ class CatalogInfo extends React.Component {
   render() {
     const { record, stripes } = this.props;
     const id = record.systemInstanceIdentifier;
+    const json = record.bibRecord;
+    if (!json) {
+      stripes.logger.log('bib', 'no bibRecord');
+      return null;
+    }
+
+    let bibRecord;
+    try {
+      bibRecord = JSON.parse(json);
+    } catch (e) {
+      stripes.logger.log('bib', `could not parse bibRecord '${json}':`, e);
+      return null;
+    }
+
+    stripes.logger.log('bib', 'bibRecord =', bibRecord);
+    const title = bibRecord.title;
+    const author = get(bibRecord, 'contributors[0].name');
+    const date = get(bibRecord, 'publication[0].dateOfPublication');
+
+    // XXX I have not seen either of these fields in the wild
+    const hasISSN = !!bibRecord.issn;
+    const idKey = `ui-rs.information.${hasISSN ? 'issn' : 'isbn'}`;
+    const idValue = bibRecord[hasISSN ? 'issn' : 'isbn'];
 
     return (
       <Accordion
@@ -44,14 +69,42 @@ class CatalogInfo extends React.Component {
           headerClass={css.catalogCardHeader}
         >
           <Row>
-            <Col xs={12}>
+            <Col xs={12} style={{ textAlign: 'right' }}>
               <a
                 target="_blank"
                 rel="noopener noreferrer"
                 href={`${stripes.config.sharedIndexUI}/inventory/view/${id}`}
               >
-                [Link to Shared Index]
+                View Record
               </a>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6}>
+              <KeyValue
+                label={<FormattedMessage id="ui-rs.information.title" />}
+                value={title}
+              />
+            </Col>
+            <Col xs={6}>
+              <KeyValue
+                label={<FormattedMessage id="ui-rs.information.author" />}
+                value={author}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6}>
+              <KeyValue
+                label={<FormattedMessage id={idKey} />}
+                value={idValue}
+              />
+            </Col>
+            <Col xs={6}>
+              <KeyValue
+                label={<FormattedMessage id="ui-rs.information.date" />}
+                value={date}
+              />
             </Col>
           </Row>
         </Card>
