c0372cab5e307531185894b5217ed5880dd78a70
EthanFreestone <ethan.freestone@k-int.co.uk>
2019-12-10 14:37:30 +0000

    some tweaks to make settings more user friendly

--- src/settings/index.js
+++ src/settings/index.js
@@ -4,6 +4,8 @@ import { FormattedMessage } from 'react-intl';
 import { Settings } from '@folio/stripes/smart-components';
 import { stripesConnect } from '@folio/stripes/core';
 import SettingPage from './SettingPage';
+import { FormattedUTCDate } from '@folio/stripes-components';
+import translations from '../../translations/ui-rs/en_US.json'
 
 class ResourceSharingSettings extends React.Component {
   static manifest = Object.freeze({
@@ -35,6 +37,13 @@ class ResourceSharingSettings extends React.Component {
     const sections = this.getSectionsList();
     const pages = sections.map(section => {
       if (section) {
+        const key = `settingsSection.${section}`
+        console.log("Translation for section: ", key)
+        if (key in translations) {
+          console.log(`${key} found in translations!`)
+        } else {
+          console.log(`${key} not found in translations.`)
+        }
         const route = section.replace(' ', '_').toLowerCase();
         let label = section.replace('_', ' ').toLowerCase();
         label = label.charAt(0).toUpperCase() + label.substring(1) + ' settings';
@@ -56,71 +65,45 @@ class ResourceSharingSettings extends React.Component {
   // Whenever new sections are added, they won't show up on first render unless added here.
   staticSettingsSections = [
     {
-      label: 'General',
-      pages: [
-        {
-          route: 'shared_index',
-          label: 'Shared index settings',
-          component: this.customComponentMaker('shared_index')
-        },
-        {
-          route: 'z3950',
-          label: 'Z3950 settings',
-          component: this.customComponentMaker('z3950')
-        },
-        {
-          route: 'requests',
-          label: 'Requests settings',
-          component: this.customComponentMaker('requests')
-        },
-        {
-          route: 'requester_validation',
-          label: 'Requester validation settings',
-          component: this.customComponentMaker('Requester Validation')
-        },
-        {
-          route: 'local_ncip',
-          label: 'Local ncip settings',
-          component: this.customComponentMaker('Local NCIP')
-        },
-      ],
+      route: 'shared_index',
+      label: 'Shared index settings',
+      component: this.customComponentMaker('shared_index')
     },
     {
-      label: 'Request',
-      pages: [],
+      route: 'z3950',
+      label: 'Z3950 settings',
+      component: this.customComponentMaker('z3950')
     },
     {
-      label: 'Supply',
-      pages: [],
+      route: 'requests',
+      label: 'Requests settings',
+      component: this.customComponentMaker('requests')
     },
+    {
+      route: 'requester_validation',
+      label: 'Requester validation settings',
+      component: this.customComponentMaker('Requester Validation')
+    },
+    {
+      route: 'local_ncip',
+      label: 'Local ncip settings',
+      component: this.customComponentMaker('Local NCIP')
+    }
   ];
 
   render() {
     const pageList = this.pageList();
 
     // Doing this in render to force update once it's grabbed the sections lists
-    const dynamicSettingsSections = [
-      {
-        label: 'General',
-        pages: pageList,
-      },
-      {
-        label: 'Request',
-        pages: [],
-      },
-      {
-        label: 'Supply',
-        pages: [],
-      },
-    ];
+    const dynamicSettingsSections = pageList;
 
     if (pageList[0]) {
       return (
-        <Settings {...this.props} sections={dynamicSettingsSections} paneTitle={<FormattedMessage id="ui-rs.meta.title" />} />
+        <Settings {...this.props} pages={dynamicSettingsSections} paneTitle={<FormattedMessage id="ui-rs.meta.title" />} />
       );
     } else {
       return (
-        <Settings {...this.props} sections={this.staticSettingsSections} paneTitle={<FormattedMessage id="ui-rs.meta.title" />} />
+        <Settings {...this.props} pages={this.staticSettingsSections} paneTitle={<FormattedMessage id="ui-rs.meta.title" />} />
       );
     }
   }
