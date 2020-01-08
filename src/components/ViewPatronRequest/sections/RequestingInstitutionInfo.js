cc957b1102b2c5a65c222288ffaf8e865c437709
Mike Taylor <mike@indexdata.com>
2020-01-08 14:17:46 +0000

    Remove extraneous nested <Accordion>

--- src/components/ViewPatronRequest/sections/RequestingInstitutionInfo.js
+++ src/components/ViewPatronRequest/sections/RequestingInstitutionInfo.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
 import get from 'lodash/get';
-import { Accordion } from '@folio/stripes/components';
 import InstitutionCard from '../../cards/institution/InstitutionCard';
 
 import css from './RequestingInstitutionInfo.css';
@@ -10,25 +8,17 @@ import css from './RequestingInstitutionInfo.css';
 class RequestingInstitutionInfo extends React.Component {
   static propTypes = {
     record: PropTypes.object,
-    id: PropTypes.string,
-    closedByDefault: PropTypes.bool,
   };
 
   render() {
     const { record } = this.props;
 
     return (
-      <Accordion
-        id={this.props.id}
-        label={<FormattedMessage id="ui-rs.information.heading.requestinginstitution" />}
-        closedByDefault={this.props.closedByDefault}
-      >
-        <InstitutionCard
-          institution={get(record, 'resolvedRequester.owner')}
-          cardClass={css.institutionCard}
-          headerClass={css.institutionCardHeader}
-        />
-      </Accordion>
+      <InstitutionCard
+        institution={get(record, 'resolvedRequester.owner')}
+        cardClass={css.institutionCard}
+        headerClass={css.institutionCardHeader}
+      />
     );
   }
 }
