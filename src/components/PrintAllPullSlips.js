958ef6d30af3228004bcd36e45ab11545072f7f5
Mike Taylor <mike@indexdata.com>
2020-01-09 15:32:44 +0000

    Refactoring
    The print-or-cancel logic that was common to the <PrintPullSlip> and
    <PrintAllPullSlips> components is now factored out into
    <PrintOrCancel>, which both of them use.

--- src/components/PrintAllPullSlips.js
+++ src/components/PrintAllPullSlips.js
@@ -1,13 +1,7 @@
-// See comment on security issues in PrintPullSlip.js
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
-import { withRouter } from 'react-router';
-import ReactToPrint from 'react-to-print';
-import { Button, PaneHeaderIconButton, HotKeys } from '@folio/stripes/components';
 import AllPullSlips from './PullSlip/AllPullSlips';
-import css from './PrintPullSlip.css';
+import PrintOrCancel from './PrintOrCancel';
 
 class PrintAllPullSlips extends React.Component {
   static propTypes = {
@@ -20,19 +14,9 @@ class PrintAllPullSlips extends React.Component {
         PropTypes.object.isRequired,
       ),
     }).isRequired,
-    history: PropTypes.shape({
-      push: PropTypes.func.isRequired,
-    }).isRequired,
   };
 
-  constructor(props) {
-    super(props);
-    this.ref = React.createRef();
-  }
-
   render() {
-    const keys = { cancel: ['escape'] };
-    const handlers = { cancel: () => this.props.history.push('details') };
     const { hasLoaded, other, records } = this.props.records;
 
     if (!hasLoaded) {
@@ -44,35 +28,8 @@ class PrintAllPullSlips extends React.Component {
       return `Not enough records loaded for printing (${records.length} of ${totalRecords})`;
     }
 
-    return (
-      <HotKeys keyMap={keys} handlers={handlers}>
-        <div className={css.buttonBar}>
-          <div className={css.cancelIcon}>
-            <FormattedMessage id="ui-rs.button.cancel-print">
-              {ariaLabel => (
-                <PaneHeaderIconButton
-                  icon="times"
-                  to="details"
-                  aria-label={ariaLabel}
-                />
-              )}
-            </FormattedMessage>
-          </div>
-          <ReactToPrint
-            trigger={() => (
-              <Button data-test-print-pull-slip marginBottom0>
-                <FormattedMessage id="ui-rs.button.print" />
-              </Button>
-            )}
-            content={() => this.ref.current}
-          />
-        </div>
-        <div ref={this.ref}>
-          <AllPullSlips records={records} />
-        </div>
-      </HotKeys>
-    );
+    return <PrintOrCancel><AllPullSlips records={records} /></PrintOrCancel>;
   }
 }
 
-export default withRouter(PrintAllPullSlips);
+export default PrintAllPullSlips;
