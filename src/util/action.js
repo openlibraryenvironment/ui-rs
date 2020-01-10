fa4f0a8fe7cc0e3cd4e3a6f0d7cc99affc185aa9
Mike Taylor <mike@indexdata.com>
2020-01-10 18:02:31 +0000

    Comment out unused performActionViaManifest
    Because ESLint complains about it.

--- src/util/action.js
+++ src/util/action.js
@@ -1,7 +1,5 @@
 // An attempt to abstract out the business of posting actions to mod-rs
 
-import { useMessage } from '../components/MessageModalState';
-
 export const actionManifestEntry = {
   type: 'okapi',
   path: 'rs/patronrequests/:{id}/performAction',
@@ -9,11 +7,14 @@ export const actionManifestEntry = {
   clientGeneratePk: false,
 };
 
+/*
 // I wanted to use performActionViaManifest in FlowRoute.js and my own app, as:
 //   const performAction = (a, p, s, e) => performActionViaManifest(props.mutator.action, a, p, s, e);
 // But this fails because hooks are too magical and can only be invoked from within functional components.
 // So I don't at present have a good way to abstract our the action-posting behaviour.
 // The best we can do is the rather trivial postAction function
+
+import { useMessage } from '../components/MessageModalState';
 
 export const performActionViaManifest = (mutator, action, payload, successMessage, errorMessage) => {
   const [, setMessage] = useMessage();
@@ -22,8 +23,8 @@ export const performActionViaManifest = (mutator, action, payload, successMessag
     .then(() => setMessage(successMessage, 'success'))
     .catch(() => setMessage(errorMessage, 'error'));
 };
+*/
 
 export const postAction = (mutator, action, payload) => {
-  console.log('postAction, payload =', payload);
   return mutator.POST({ action, actionParams: payload || {} });
 };
