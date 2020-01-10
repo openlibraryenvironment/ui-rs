65d6ef0842189f62fc834703b0b1e382482151dd
Mike Taylor <mike@indexdata.com>
2020-01-10 18:10:05 +0000

    Towards PR-432, clicks not registering yet

--- src/components/PrintOrCancel.js
+++ src/components/PrintOrCancel.js
@@ -26,6 +26,7 @@ class PrintOrCancel extends React.Component {
       push: PropTypes.func.isRequired,
     }).isRequired,
     destUrl: PropTypes.string.isRequired,
+    onPrint: PropTypes.func,
     children: PropTypes.element.isRequired,
   };
 
@@ -54,7 +55,7 @@ class PrintOrCancel extends React.Component {
           </div>
           <ReactToPrint
             trigger={() => (
-              <Button data-test-print-pull-slip marginBottom0>
+              <Button data-test-print-pull-slip marginBottom0 onClick={this.props.onPrint}>
                 <FormattedMessage id="ui-rs.button.print" />
               </Button>
             )}
