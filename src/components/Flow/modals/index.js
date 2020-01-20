7d3615a6557fcf60fc9898bc3ed77e5438aac022
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-01-20 09:07:07 +0000

    Added requesterReceived modal back in, now with modal file actually added to git

--- src/components/Flow/modals/index.js
+++ src/components/Flow/modals/index.js
@@ -2,3 +2,4 @@ export const Cancel = () => null;
 export { default as CannotSupply } from './CannotSupply';
 export { default as SendChatMessage } from './SendChatMessage';
 export { default as RespondYes } from './RespondYes';
+export { default as RequesterReceived } from './RequesterReceived';
