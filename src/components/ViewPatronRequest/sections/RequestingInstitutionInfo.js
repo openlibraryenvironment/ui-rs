7073eb73f64e4fdced14a00db3b7de98eb2554e5
Mike Taylor <mike@indexdata.com>
2020-01-24 12:33:08 +0000

    Move InstitutionCard into the component that uses it

--- src/components/ViewPatronRequest/sections/RequestingInstitutionInfo.js
+++ src/components/ViewPatronRequest/sections/RequestingInstitutionInfo.js
@@ -1,20 +1,133 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import stringify from 'json-stable-stringify';
+import { withStripes } from '@folio/stripes/core';
 import get from 'lodash/get';
-import InstitutionCard from './InstitutionCard';
+import {
+  Accordion,
+  Card,
+  Col,
+  KeyValue,
+  Row,
+} from '@folio/stripes/components';
 
 import css from './RequestingInstitutionInfo.css';
 
-class RequestingInstitutionInfo extends React.Component {
+class InstitutionCard extends React.Component {
   static propTypes = {
-    record: PropTypes.object,
+    institution: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+    stripes: PropTypes.shape({
+      config: PropTypes.shape({
+        showDevInfo: PropTypes.bool,
+      }).isRequired,
+    }).isRequired,
   };
 
   render() {
-    const { record } = this.props;
+    const props = Object.assign({}, this.props);
+    // React complains if any of these props are passed in <Card>
+    delete props.refreshRemote;
+    delete props.dataKey;
+    const institutionSymbol = props.institutionSymbol;
+    delete props.institutionSymbol;
+
+    let institution = props.institution;
+    if (institution) {
+      props.cardStyle = 'positive';
+    } else {
+      props.cardStyle = 'negative';
+      delete props.headerClass;
+      delete props.cardClass;
+      institution = {};
+    }
+
+    return (
+      <Card
+        id="requestingInstitutionInfo-card"
+        headerStart="Institution"
+        roundedBorder
+        {...props}
+      >
+        <Row>
+          <Col xs={6}>
+            <KeyValue
+              label={<FormattedMessage id="ui-rs.information.institutionId" />}
+              value={institution.id || institutionSymbol}
+            />
+          </Col>
+          <Col xs={6}>
+            <KeyValue
+              label={<FormattedMessage id="ui-rs.information.institutionName" />}
+              value={institution.name}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6}>
+            <KeyValue
+              label={<FormattedMessage id="ui-rs.information.institutionStatus" />}
+              value={get(institution.status, ['label'])}
+            />
+          </Col>
+          <Col xs={6}>
+            <KeyValue
+              label={<FormattedMessage id="ui-rs.information.slug" />}
+              value={institution.slug}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <KeyValue
+              label={<FormattedMessage id="ui-rs.information.symbols" />}
+              value={institution.symbolSummary}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <KeyValue
+              label={<FormattedMessage id="ui-rs.information.tags" />}
+              value={institution.tagSummary}
+            />
+          </Col>
+        </Row>
+        {!props.stripes.config.showDevInfo ? '' :
+        <Row>
+          <Col xs={12}>
+            <Accordion
+              id="requestingInstitutionInfo-card-dev"
+              label={<FormattedMessage id="ui-rs.information.heading.developer" />}
+              closedByDefault
+            >
+              <pre>
+                {stringify(institution, { space: 2 })}
+              </pre>
+            </Accordion>
+          </Col>
+        </Row>
+        }
+      </Card>
+    );
+  }
+}
+
+// eslint-disable-next-line react/no-multi-comp
+class RequestingInstitutionInfo extends React.Component {
+  static propTypes = {
+    record: PropTypes.object,
+    stripes: PropTypes.object,
+  };
+
+  render() {
+    const { record, stripes } = this.props;
 
     return (
       <InstitutionCard
+        stripes={stripes}
         institution={get(record, 'resolvedRequester.owner')}
         cardClass={css.institutionCard}
         headerClass={css.institutionCardHeader}
@@ -23,4 +136,4 @@ class RequestingInstitutionInfo extends React.Component {
   }
 }
 
-export default RequestingInstitutionInfo;
+export default withStripes(RequestingInstitutionInfo);
