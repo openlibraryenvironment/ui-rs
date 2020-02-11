c7a8b8a7ec2981afdee6d8aaacd4d3aac0e0a6cd
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-02-11 09:38:42 +0000

    refactored addNoteField as a functional component

--- src/components/AddNoteField.js
+++ src/components/AddNoteField.js
@@ -1,42 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Field } from 'react-final-form';
 import { Button, Col, Row, TextField } from '@folio/stripes/components';
 import { FormattedMessage } from 'react-intl';
 
-class AddNoteForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { noteFieldOpen: false };
-  }
+const AddNoteForm = () => {
+  const [noteFieldOpen, setnoteFieldOpen] = useState(false);
 
-  renderMessageField() {
-    const { noteFieldOpen } = this.state;
+  const renderMessageField = () => {
     if (noteFieldOpen === true) {
       return (
         <Field name="note" component={TextField} />
       );
     }
     return null;
-  }
+  };
 
-  render() {
-    const { noteFieldOpen } = this.state;
-    return (
-      <Row>
-        <Col xs={4}>
-          <Button onClick={() => {
-            this.setState({ noteFieldOpen: !noteFieldOpen });
-          }}
-          >
-            {noteFieldOpen ? <FormattedMessage id="ui-rs.actions.hideNoteField" /> : <FormattedMessage id="ui-rs.actions.addNote" />}
-          </Button>
-        </Col>
-        <Col xs={8}>
-          {this.renderMessageField()}
-        </Col>
-      </Row>
-    );
-  }
+  return (
+    <Row>
+      <Col xs={4}>
+        <Button onClick={() => {
+          setnoteFieldOpen(!noteFieldOpen);
+        }}
+        >
+          {noteFieldOpen ? <FormattedMessage id="ui-rs.actions.hideNoteField" /> : <FormattedMessage id="ui-rs.actions.addNote" />}
+        </Button>
+      </Col>
+      <Col xs={8}>
+        {renderMessageField()}
+      </Col>
+    </Row>
+  );
 }
 
 export default AddNoteForm;
