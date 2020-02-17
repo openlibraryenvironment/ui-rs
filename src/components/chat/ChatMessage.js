d8a6f8e7f70dc4ff07bb5b58c1f3542ee662b698
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-02-17 13:44:41 +0000

    changes to the way timestamps are rendered in the chatpane

--- src/components/chat/ChatMessage.js
+++ src/components/chat/ChatMessage.js
@@ -4,32 +4,77 @@ import { Form, Field } from 'react-final-form';
 import { FormattedDate, FormattedMessage } from 'react-intl';
 import { stripesConnect } from '@folio/stripes/core';
 import { Button, Card, Col, IconButton, Pane, Row, TextField, Tooltip } from '@folio/stripes/components';
+import moment from 'moment';
 import css from './ChatMessage.css';
 
 
 class ChatMessage extends React.Component {
+  static propTypes = {
+    notification: PropTypes.shape({
+      id: PropTypes.string,
+      messageContent: PropTypes.string,
+      timestamp: PropTypes.string,
+      seen: PropTypes.bool,
+      isSender: PropTypes.bool,
+      attachedAction: PropTypes.string,
+      messageReceiver: PropTypes.shape({
+        id: PropTypes.string,
+        owner: PropTypes.shape({
+          id: PropTypes.string,
+          name: PropTypes.string,
+        })
+      }),
+      messageSender: PropTypes.shape({
+        id: PropTypes.string,
+        owner: PropTypes.shape({
+          id: PropTypes.string,
+          name: PropTypes.string,
+        })
+      })
+    })
+  }
 
   renderDateTime(timestamp) {
-    return (
-      <FormattedDate
-        value={timestamp}
-        day="numeric"
-        month="numeric"
-        year="numeric"
-        hour="numeric"
-        minute="numeric"
-      />
-    );
+    const currentTime = moment();
+    const timestampDate = moment(timestamp);
+
+    const duration = moment.duration(currentTime.diff(timestamp));
+
+    const days = duration?._data.days;
+    const minutes = duration?._data.minutes;
+    const seconds = duration?._data.seconds;
+
+    console.log(duration)
+
+    if (days === 0) {
+      if (minutes === 0) {
+        return <FormattedMessage id="ui-rs.view.chatMessage.justNow" />
+      } else if (minutes === 1) {
+        return <FormattedMessage id="ui-rs.view.chatMessage.minute" />;
+      } else {
+        return <FormattedMessage id="ui-rs.view.chatMessage.minutes" values={{ minutes }} />;
+      }
+    } else {
+      return (
+        <FormattedDate
+          value={timestamp}
+          day="numeric"
+          month="numeric"
+          year="numeric"
+          hour="numeric"
+          minute="numeric"
+        />
+      );
+    }
   }
 
   render() {
-    const { notification } = this.props
+    const { notification } = this.props;
 
     console.log("Notification: %o", notification)
     return (
       <>
-        <p>Hello</p>
-        {this.renderDateTime(notification?.timestamp)}
+        <b>{notification?.messageSender?.owner?.name} </b> {this.renderDateTime(notification?.timestamp)}
         <hr />
       </>
     );
