9d13dd1301704342dec0d9c972f01480c2eb2a2c
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2020-03-02 09:38:32 +0000

    Settings Refactor (#17)
    * refactored settings list to make it use a similar structure to other ui-rs forms (hopefully fixing a bug in the process)
    
    * fixed indentation error elsewhere in the app

--- src/settings/settingsComponents/EditableSettingsList.js
+++ src/settings/settingsComponents/EditableSettingsList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FieldArray } from 'react-final-form-arrays';
+import { Form } from 'react-final-form';
+import arrayMutators from 'final-form-arrays';
 
 import { Pane } from '@folio/stripes/components';
-import stripesFinalForm from '@folio/stripes/final-form';
 import { FormattedMessage } from 'react-intl';
 
 import EditableSettingsListFieldArray from './EditableSettingsListFieldArray';
@@ -11,7 +12,6 @@ import EditableSettingsListFieldArray from './EditableSettingsListFieldArray';
 class EditableSettingsList extends React.Component {
   static propTypes = {
     onSave: PropTypes.func,
-    form: PropTypes.object,
     data: PropTypes.shape({
       refdatavalues: PropTypes.arrayOf(PropTypes.object)
     }),
@@ -25,42 +25,46 @@ class EditableSettingsList extends React.Component {
 
   render() {
     const {
-      form: { mutators },
       data
     } = this.props;
     return (
-      <Pane
-        defaultWidth="fill"
-        id={`settings-${this.props.settingSection}`}
-        paneTitle={<FormattedMessage id={`ui-rs.settingsSection.${this.props.settingSection}`} />}
+      <Form
+        onSubmit={this.handleSave}
+        initialValues={this.props.initialValues}
+        enableReinitialize
+        keepDirtyOnReinitialize
+        mutators={{
+          setSettingValue: (args, state, tools) => {
+            tools.changeValue(state, args[0], () => args[1]);
+          },
+          ...arrayMutators
+        }}
+        subscription={{ value: true }}
+        navigationCheck
       >
-        <form>
-          <FieldArray
-            component={EditableSettingsListFieldArray}
-            name="settings"
-            onSave={this.handleSave}
-            mutators={mutators}
-            data={{
-              refdatavalues: data?.refdatavalues
-            }}
-            initialValues={this.props.initialValues}
-          />
-        </form>
-      </Pane>
+        {({ handleSubmit, mutators }) => (
+          <Pane
+            defaultWidth="fill"
+            id={`settings-${this.props.settingSection}`}
+            paneTitle={<FormattedMessage id={`ui-rs.settingsSection.${this.props.settingSection}`} />}
+          >
+            <form onSubmit={handleSubmit}>
+              <FieldArray
+                component={EditableSettingsListFieldArray}
+                name="settings"
+                onSave={this.handleSave}
+                mutators={mutators}
+                data={{
+                  refdatavalues: data?.refdatavalues
+                }}
+                initialValues={this.props.initialValues}
+              />
+            </form>
+          </Pane>
+        )}
+      </Form>
     );
   }
 }
 
-export default stripesFinalForm({
-  enableReinitialize: true,
-  keepDirtyOnReinitialize: false,
-  mutators: {
-    setSettingValue: (args, state, tools) => {
-      tools.changeValue(state, args[0], () => args[1]);
-    },
-  },
-  subscription: {
-    value: true
-  },
-  navigationCheck: true,
-})(EditableSettingsList);
+export default EditableSettingsList;
