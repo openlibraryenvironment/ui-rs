2322761deabe6c8d8702f23f19d4217cd9933407
Mike Taylor <mike@indexdata.com>
2020-04-16 17:13:39 +0100

    Fix a bunch of ESLint errors in NoteForm
    I have left two more complicated ones in the area of accessibility

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { Form, Field } from 'react-final-form';
 import PropTypes from 'prop-types';
 import { Button, TextField } from '@folio/stripes/components';
-import stripesFinalForm from '@folio/stripes/final-form';
 import { FormattedMessage } from 'react-intl';
-import classNames from 'classnames';
 
 import css from './NoteForm.css';
 
@@ -12,62 +10,68 @@ class AddNoteForm extends React.Component {
   static propTypes = {
     onSend: PropTypes.func.isRequired,
     setVisibility: PropTypes.func.isRequired,
-    visibility: PropTypes.bool.isRequired
+    visibility: PropTypes.bool.isRequired,
+    className: PropTypes.string,
   };
 
   constructor(props) {
     super(props);
-    this.state = { noteValue: null }
+    this.state = { noteValue: null };
   }
-  
-  stopPropagation = event => { event.preventDefault();event.stopPropagation(); }
-  
-  handleSend = ( e ) => {
-    this.stopPropagation ( e );
+
+  stopPropagation = event => { event.preventDefault(); event.stopPropagation(); }
+
+  handleSend = (e) => {
+    this.stopPropagation(e);
     const val = this.state.noteValue?.trim();
-    this.props.onSend( val ?? null );
+    this.props.onSend(val ?? null);
   }
-  
+
   handleOnBlur = () => {
     const val = this.state.noteValue?.trim() ?? '';
-    if (val == '') {
-      this.props.setVisibility( false );
+    if (val === '') {
+      this.props.setVisibility(false);
     }
   }
 
   render() {
     const { className, setVisibility, visibility } = this.props;
-    
+
     if (visibility) {
       // Render the form
       return (
         <Form
-          onSubmit={ this.stopPropagation }
+          onSubmit={this.stopPropagation}
           render={({ handleSubmit }) => (
             <form
-              onSubmit={ handleSubmit }
-              onClick={ this.stopPropagation }
+              onSubmit={handleSubmit}
+              onClick={this.stopPropagation}
               autoComplete="off"
               className={className}
             >
-              <Field name="note" component={TextField} onBlur={this.handleOnBlur} autoFocus onChange={ (e)=>{this.setState({noteValue : e.target.value}); }} />
+              <Field name="note" component={TextField} onBlur={this.handleOnBlur} autoFocus onChange={(e) => { this.setState({ noteValue : e.target.value }); }} />
               <Button
-                onClick={ this.handleSend }
-                onBlur={ this.handleOnBlur }
+                onClick={this.handleSend}
+                onBlur={this.handleOnBlur}
               >
                 <FormattedMessage id="ui-rs.actions.send" />
               </Button>
             </form>
-          )} />
+          )}
+        />
       );
     } else {
-      return (<Button buttonClass={css.addNoteButton}
-        onClick={( event ) => {
-          this.stopPropagation( event );
-          setVisibility( true );
-        }}
-        ><FormattedMessage id="ui-rs.actions.addNote" />
-      </Button>);
+      return (
+        <Button
+          buttonClass={css.addNoteButton}
+          onClick={(event) => {
+            this.stopPropagation(event);
+            setVisibility(true);
+          }}
+        >
+          <FormattedMessage id="ui-rs.actions.addNote" />
+        </Button>
+      );
     }
   }
 }
