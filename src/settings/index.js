3c93b5a6bba0c3d367e33942cbd45cf65412d839
Mike Taylor <mike@indexdata.com>
2020-05-07 15:00:06 +0100

    Simplify settings index page (#31)

--- src/settings/index.js
+++ src/settings/index.js
@@ -6,6 +6,14 @@ import { stripesConnect } from '@folio/stripes/core';
 import SettingPage from './SettingPage';
 import { CustomISO18626 } from './settingsComponents';
 
+
+function routeAlphaSort(a, b) {
+  if (a.route < b.route) return -1;
+  if (a.route > b.route) return 1;
+  return 0;
+}
+
+
 class ResourceSharingSettings extends React.Component {
   static manifest = Object.freeze({
     settings: {
@@ -25,91 +33,23 @@ class ResourceSharingSettings extends React.Component {
     }),
   };
 
-  getSectionsList() {
+  pageList() {
     const rows = (this.props.resources.settings ? this.props.resources.settings.records : []);
     const sections = Array.from(new Set(rows.map(obj => obj.section)));
-    return (sections);
-  }
-
-  customComponentMaker(sectionName) {
-    return (props) => <SettingPage sectionName={sectionName} {...props} />;
-  }
-
-  routeAlphaSort = (a, b) => {
-    if (a.route < b.route) {
-      return -1;
-    }
-    if (a.route > b.route) {
-      return 1;
-    }
-    return 0;
-  };
-
-  pageList() {
-    const sections = this.getSectionsList();
-    const pages = sections.map(section => {
+    return sections.map(section => {
       if (section) {
         const route = section;
         const label = <FormattedMessage id={`ui-rs.settingsSection.${section}`} />;
-        return (
-          {
-            'route': route,
-            'label': label,
-            'component': this.customComponentMaker(section)
-          }
-        );
+        return {
+          'route': route,
+          'label': label,
+          'component': (props) => <SettingPage sectionName={section} {...props} />,
+        };
       } else {
         return (undefined);
       }
     });
-    return (pages.sort((a, b) => this.routeAlphaSort(a, b)));
   }
-
-  // Backup pages for initial render (Settings doesn't render dynamically properly at first).
-  // Whenever new sections are added, they won't show up on first render unless added here.
-  // They should be in alphabetical order (by route) here, to match the sorted dynamic list.
-  staticSettingsPages = [
-    {
-      route: 'AutoResponder',
-      label: 'Auto responder settings',
-      component: this.customComponentMaker('autoResponder')
-    },
-    {
-      route: 'Chat',
-      label: 'Chat settings',
-      component: this.customComponentMaker('chat')
-    },
-    {
-      route: 'HostLMSIntegration',
-      label: 'Host LMS integration',
-      component: this.customComponentMaker('hostLMSIntegration')
-    },
-    {
-      route: 'localNCIP',
-      label: 'Local NCIP settings',
-      component: this.customComponentMaker('localNCIP')
-    },
-    {
-      route: 'requesterValidation',
-      label: 'Requester validation',
-      component: this.customComponentMaker('requesterValidation')
-    },
-    {
-      route: 'requests',
-      label: 'Request defaults',
-      component: this.customComponentMaker('requests')
-    },
-    {
-      route: 'sharedIndex',
-      label: 'Shared index settings',
-      component: this.customComponentMaker('sharedIndex')
-    },
-    {
-      route: 'z3950',
-      label: 'Z39.50 settings',
-      component: this.customComponentMaker('z3950')
-    }
-  ];
 
   persistentPages = [
     {
@@ -121,20 +61,10 @@ class ResourceSharingSettings extends React.Component {
 
   render() {
     const pageList = this.pageList();
+    if (!pageList[0]) return null; // XXX Removing this line breaks the render!
 
-    // Doing this in render to force update once it's grabbed the page list
-    const dynamicSettingsPages = pageList;
-    let settingsToRender = [];
-
-    if (pageList[0]) {
-      settingsToRender = this.persistentPages.concat(dynamicSettingsPages).sort((a, b) => this.routeAlphaSort(a, b));
-    } else {
-      settingsToRender = this.persistentPages.concat(this.staticSettingsPages).sort((a, b) => this.routeAlphaSort(a, b));
-    }
-
-    return (
-      <Settings {...this.props} pages={settingsToRender} paneTitle={<FormattedMessage id="ui-rs.meta.title" />} />
-    );
+    const settingsToRender = this.persistentPages.concat(pageList).sort(routeAlphaSort);
+    return <Settings {...this.props} pages={settingsToRender} paneTitle={<FormattedMessage id="ui-rs.meta.title" />} />;
   }
 }
 
