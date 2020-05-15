68ce1119d51e9a6fc2afeee9e4eebcf9d4405ef6
Mike Taylor <mike@indexdata.com>
2020-05-15 18:30:02 +0100

    First stage of rendering pullslip notifications

--- src/settings/settingsComponents/PullslipNotifications.js
+++ src/settings/settingsComponents/PullslipNotifications.js
@@ -1,16 +1,59 @@
 import React from 'react';
-import { Paneset, Pane } from '@folio/stripes/components';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+import { FormattedMessage } from 'react-intl';
+import { Paneset, Pane, MultiColumnList } from '@folio/stripes/components';
+import hardwiredSampleData from './sample';
+
 
 class PullslipNotifications extends React.Component {
+  static propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
+  };
+
+  onRowClick = (_event, record) => {
+    console.log('clicked row:', record);
+    this.props.history.push(record.id);
+  }
+
   render() {
     return (
       <Paneset>
-        <Pane>
-          Pull-slip notification maintenance goes here!
+        <Pane defaultWidth="100%">
+          <MultiColumnList
+            autosize
+            contentData={hardwiredSampleData}
+            visibleColumns={['status', 'name', 'days', 'times', 'locations', 'emailAddresses']}
+            columnMapping={{
+              status: <FormattedMessage id="ui-rs.pullslipNotification.column.status" />,
+              name: <FormattedMessage id="ui-rs.pullslipNotification.column.name" />,
+              days: <FormattedMessage id="ui-rs.pullslipNotification.column.days" />,
+              times: <FormattedMessage id="ui-rs.pullslipNotification.column.times" />,
+              locations: <FormattedMessage id="ui-rs.pullslipNotification.column.locations" />,
+              emailAddresses: <FormattedMessage id="ui-rs.pullslipNotification.column.emailAddresses" />,
+            }}
+            columnWidths={{
+              status: 60,
+              name: 150,
+              days: 150,
+              times: 150,
+              locations: 250,
+              emailAddresses: 400,
+            }}
+            formatter={{
+              days: r => r.days.join(', '),
+              times: r => r.times.join(', '),
+              locations: r => (r.locations || []).map(l => l.substr(0, 8)).join(', '),
+              emailAddresses: r => r.emailAddresses.join(', '),
+            }}
+            onRowClick={this.onRowClick}
+          />
         </Pane>
       </Paneset>
     );
   }
 }
 
-export default PullslipNotifications;
+export default withRouter(PullslipNotifications);
