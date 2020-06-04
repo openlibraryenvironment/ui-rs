cf60c98306ca730d981515f33805a358c1d883fc
Mike Taylor <mike@indexdata.com>
2020-06-04 13:29:34 +0100

    Use functional component style instead of class

--- src/settings/pullslipNotifications/ListOfTimePicker.js
+++ src/settings/pullslipNotifications/ListOfTimePicker.js
@@ -4,33 +4,27 @@ import { Field } from 'react-final-form';
 import { FieldArray } from 'react-final-form-arrays';
 import { RepeatableField, Timepicker } from '@folio/stripes/components';
 
-
-class ListOfTimePicker extends React.Component {
-  render() {
-    const { name, legend, addLabel, timeZone } = this.props;
-
-    return (
-      <FieldArray
-        name={name}
-        legend={legend}
-        addLabel={addLabel}
-        component={RepeatableField}
-        onAdd={fields => fields.push('')}
-        renderField={field => (
-          <Field
-            component={(props) => <Timepicker {...props} timeZone={timeZone} />}
-            name={field}
-          />
-        )}
-      />
-    );
-  }
-}
+const ListOfTimePicker = (props) => <FieldArray
+  name={props.name}
+  legend={props.legend}
+  addLabel={props.addLabel}
+  component={RepeatableField}
+  onAdd={fields => fields.push('')}
+  renderField={field => (
+    <Field
+      component={(props2) => <Timepicker {...props2} timeZone={props.timeZone} />}
+      name={field}
+    />
+  )}
+/>;
 
 ListOfTimePicker.propTypes = {
   name: PropTypes.string.isRequired,
   legend: PropTypes.string,
-  addLabel: PropTypes.string,
+  addLabel: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element,
+  ]),
   timeZone: PropTypes.string,
 };
 
