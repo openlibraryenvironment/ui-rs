80c1fff9218180203270215788baab85c7a1359a
Jason Skomorowski <jason@indexdata.com>
2020-08-16 19:10:01 -0400

    Refactor ActionButton as function so I can use useContext
    Also fixed bug where it mutated the payload prop.

--- src/components/Flow/ActionButton.js
+++ src/components/Flow/ActionButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Button, Icon } from '@folio/stripes/components';
@@ -8,79 +8,63 @@ import css from './ActionButton.css';
 import NoteForm from '../NoteForm';
 import { includesNote } from './actionsByState';
 
-class ActionButton extends Component {
-  static propTypes = {
-    label: PropTypes.string.isRequired,
-    icon: PropTypes.string,
-    action: PropTypes.string.isRequired,
-    payload: PropTypes.object,
-    success: PropTypes.string,
-    error: PropTypes.string,
-    performAction: PropTypes.func.isRequired,
+const ActionButton = ({ action, performAction, payload = {}, success = null, error = null, icon = null, label }) => {
+  const [noteFieldOpen, setNoteFieldOpen] = useState(false);
+  const onSubmitNote = (note) => {
+    performAction(action, { ...payload, note }, success, error);
   };
 
-  constructor(props) {
-    super(props);
-    this.state = { noteFieldOpen: false };
-  }
-
-  onSubmitNote = (note) => {
-    const { action, success, error, performAction } = this.props;
-    const payload = this.props.payload || {};
-    payload.note = note;
-    performAction(action, payload, success, error);
-  };
-
-  setNoteFormVisibility = visibility => {
-    this.setState({ noteFieldOpen: visibility });
-  }
-
-  handleClick = (e) => {
+  const handleClick = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    const { action, success, error, performAction, payload } = this.props;
 
     // Stop this event from firing if the notes form is shown.
-    if (!this.state.noteFieldOpen) {
+    if (!noteFieldOpen) {
       performAction(action, payload, success, error);
     }
     // else NOOP.
-  }
+  };
 
-  render() {
-    const { icon, label, action } = this.props;
-    const withNote = (action ? includesNote[action] : null) ?? includesNote.default;
-    const { noteFieldOpen } = this.state;
+  const withNote = (action ? includesNote[action] : null) ?? includesNote.default;
 
-    if (!noteFieldOpen) {
-      return (
-        <span className={`${css.actionWrapper} ${interCss.interactionStyles}`}>
-          <Button
-            buttonStyle="dropdownItem"
-            onClick={this.handleClick}
-            buttonClass={{ [`${css.actionButton}`] : true, [`${css.withInlineForm}`]: withNote }}
-          >
-            <Icon icon={icon || 'default'} className={css.button}>
-              <FormattedMessage id={label} />
-            </Icon>
-          </Button>
-          { withNote && <NoteForm onSend={this.onSubmitNote} visibility={noteFieldOpen} setVisibility={this.setNoteFormVisibility} className={css.addNoteForm} /> }
+  if (!noteFieldOpen) {
+    return (
+      <span className={`${css.actionWrapper} ${interCss.interactionStyles}`}>
+        <Button
+          buttonStyle="dropdownItem"
+          onClick={handleClick}
+          buttonClass={{ [`${css.actionButton}`] : true, [`${css.withInlineForm}`]: withNote }}
+        >
+          <Icon icon={icon || 'default'} className={css.button}>
+            <FormattedMessage id={label} />
+          </Icon>
+        </Button>
+        { withNote && <NoteForm onSend={onSubmitNote} visibility={noteFieldOpen} setVisibility={setNoteFieldOpen} className={css.addNoteForm} /> }
+      </span>
+    );
+  } else {
+    // Form is open replace the button with a span.
+    return (
+      <span className={`${css.actionWrapper} ${interCss.interactionStyles}`}>
+        <span className={css.inlineFormWrapper}>
+          <Icon icon={icon || 'default'} className={css.button}>
+            <FormattedMessage id={label} />
+          </Icon>
         </span>
-      );
-    } else {
-      // Form is open replace the button with a span.
-      return (
-        <span className={`${css.actionWrapper} ${interCss.interactionStyles}`}>
-          <span className={css.inlineFormWrapper}>
-            <Icon icon={icon || 'default'} className={css.button}>
-              <FormattedMessage id={label} />
-            </Icon>
-          </span>
-          <NoteForm onSend={this.onSubmitNote} visibility={noteFieldOpen} setVisibility={this.setNoteFormVisibility} className={css.addNoteForm} />
-        </span>
-      );
-    }
+        <NoteForm onSend={onSubmitNote} visibility={noteFieldOpen} setVisibility={setNoteFieldOpen} className={css.addNoteForm} />
+      </span>
+    );
   }
-}
+};
+
+ActionButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  action: PropTypes.string.isRequired,
+  payload: PropTypes.object,
+  success: PropTypes.string,
+  error: PropTypes.string,
+  performAction: PropTypes.func.isRequired,
+};
 
 export default ActionButton;
