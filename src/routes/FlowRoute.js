2f7855bc964c3547b7681cbd433cbd4ee2125813
Mike Taylor <mike@indexdata.com>
2020-08-17 18:52:24 +0100

    Only show "More options" if user has `ui-${appName}.edit` permission
    Part of PR-359/PR-360.

--- src/routes/FlowRoute.js
+++ src/routes/FlowRoute.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import _ from 'lodash';
+import { withStripes } from '@folio/stripes/core';
 import { Headline, KeyValue, Layout } from '@folio/stripes/components';
+import AppNameContext from '../AppNameContext';
 import initialToUpper from '../util/initialToUpper';
 import renderNamedWithProps from '../util/renderNamedWithProps';
 import { actionsForRequest } from '../components/Flow/actionsByState';
@@ -11,39 +13,47 @@ import * as primaryActions from '../components/Flow/primaryActions';
 import * as moreActions from '../components/Flow/moreActions';
 import css from './FlowRoute.css';
 
-const FlowRoute = ({ request, performAction }) => {
+const FlowRoute = ({ request, performAction, stripes }) => {
   const forCurrent = actionsForRequest(request);
   let PrimaryAction;
   if (forCurrent.primaryAction) {
     PrimaryAction = _.get(primaryActions, initialToUpper(forCurrent.primaryAction))
       || primaryActions.Generic;
   }
+
   return (
-    <React.Fragment>
-      <Layout className="centered" style={{ maxWidth: '80em' }}>
-        <KeyValue label={<FormattedMessage id="stripes-reshare.requestState" />}>
-          <Headline size="large" faded><FormattedMessage id={`stripes-reshare.states.${request.state?.code}`} /></Headline>
-        </KeyValue>
-        <div className={css.cards}>
-          {renderNamedWithProps(forCurrent.cards, cards, { request })}
-        </div>
-        <Layout className="padding-top-gutter">
-          {PrimaryAction && <PrimaryAction request={request} name={forCurrent.primaryAction} performAction={performAction} /> }
-        </Layout>
-        {forCurrent.moreActions.length > 0 &&
-          <Layout className={`padding-top-gutter ${css.optionList} ${css.noBorderRadius}`}>
-            <strong>More options:</strong>
-            {renderNamedWithProps(forCurrent.moreActions, moreActions, { request, performAction }, moreActions.Generic)}
+    <AppNameContext.Consumer>
+      {appName => (
+        <React.Fragment>
+          <Layout className="centered" style={{ maxWidth: '80em' }}>
+            <KeyValue label={<FormattedMessage id="stripes-reshare.requestState" />}>
+              <Headline size="large" faded><FormattedMessage id={`stripes-reshare.states.${request.state?.code}`} /></Headline>
+            </KeyValue>
+            <div className={css.cards}>
+              {renderNamedWithProps(forCurrent.cards, cards, { request })}
+            </div>
+            <Layout className="padding-top-gutter">
+              {PrimaryAction && <PrimaryAction request={request} name={forCurrent.primaryAction} performAction={performAction} /> }
+            </Layout>
+            {stripes.hasPerm(`ui-${appName}.edit`) && forCurrent.moreActions.length > 0 &&
+              <Layout className={`padding-top-gutter ${css.optionList} ${css.noBorderRadius}`}>
+                <strong>More options:</strong>
+                {renderNamedWithProps(forCurrent.moreActions, moreActions, { request, performAction }, moreActions.Generic)}
+              </Layout>
+            }
           </Layout>
-        }
-      </Layout>
-    </React.Fragment>
+        </React.Fragment>
+      )}
+    </AppNameContext.Consumer>
   );
 };
 
 FlowRoute.propTypes = {
+  stripes: PropTypes.shape({
+    hasPerm: PropTypes.func.isRequired,
+  }).isRequired,
   request: PropTypes.object.isRequired,
   performAction: PropTypes.func.isRequired,
 };
 
-export default FlowRoute;
+export default withStripes(FlowRoute);
