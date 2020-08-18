9e730f40975b22e23846e237d1c4333c88430c9b
Jason Skomorowski <jason@indexdata.com>
2020-08-18 14:32:10 -0400

    Also grey out modal buttons PR-754

--- src/components/ModalButtons.js
+++ src/components/ModalButtons.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@folio/stripes/components';
 import { useModal } from './MessageModalState';
+import { ActionContext } from './Flow/ActionContext';
 
 export const CancelModalButton = props => {
   const [, setModal] = useModal();
@@ -12,11 +13,13 @@ export const CancelModalButton = props => {
 };
 
 export const ShowModalButton = ({ modal, ...rest }) => {
+  const [actions] = useContext(ActionContext);
   const [, setModal] = useModal();
   return (
     <div>
       <Button
         onClick={() => setModal(modal)}
+        disabled={actions.pending}
         {...rest}
       />
     </div>
