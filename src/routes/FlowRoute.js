b62e416e5e7223fc5925601e3b1bcc924bb9be2c
Jason Skomorowski <jason@indexdata.com>
2020-08-17 20:05:43 -0400

    Revert "Only show "More options" if user has `ui-${appName}.edit` permission"
    This reverts commit 2f7855bc964c3547b7681cbd433cbd4ee2125813.
    
    ...because it can be done more cleanly with hooks.

--- src/routes/FlowRoute.js
+++ src/routes/FlowRoute.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import _ from 'lodash';
-import { withStripes } from '@folio/stripes/core';
 import { Headline, KeyValue, Layout } from '@folio/stripes/components';
-import AppNameContext from '../AppNameContext';
 import initialToUpper from '../util/initialToUpper';
 import renderNamedWithProps from '../util/renderNamedWithProps';
 import { actionsForRequest } from '../components/Flow/actionsByState';
@@ -13,47 +11,39 @@ import * as primaryActions from '../components/Flow/primaryActions';
 import * as moreActions from '../components/Flow/moreActions';
 import css from './FlowRoute.css';
 
-const FlowRoute = ({ request, performAction, stripes }) => {
+const FlowRoute = ({ request, performAction }) => {
   const forCurrent = actionsForRequest(request);
   let PrimaryAction;
   if (forCurrent.primaryAction) {
     PrimaryAction = _.get(primaryActions, initialToUpper(forCurrent.primaryAction))
       || primaryActions.Generic;
   }
-
   return (
-    <AppNameContext.Consumer>
-      {appName => (
-        <React.Fragment>
-          <Layout className="centered" style={{ maxWidth: '80em' }}>
-            <KeyValue label={<FormattedMessage id="stripes-reshare.requestState" />}>
-              <Headline size="large" faded><FormattedMessage id={`stripes-reshare.states.${request.state?.code}`} /></Headline>
-            </KeyValue>
-            <div className={css.cards}>
-              {renderNamedWithProps(forCurrent.cards, cards, { request })}
-            </div>
-            <Layout className="padding-top-gutter">
-              {PrimaryAction && <PrimaryAction request={request} name={forCurrent.primaryAction} performAction={performAction} /> }
-            </Layout>
-            {stripes.hasPerm(`ui-${appName}.edit`) && forCurrent.moreActions.length > 0 &&
-              <Layout className={`padding-top-gutter ${css.optionList} ${css.noBorderRadius}`}>
-                <strong>More options:</strong>
-                {renderNamedWithProps(forCurrent.moreActions, moreActions, { request, performAction }, moreActions.Generic)}
-              </Layout>
-            }
+    <React.Fragment>
+      <Layout className="centered" style={{ maxWidth: '80em' }}>
+        <KeyValue label={<FormattedMessage id="stripes-reshare.requestState" />}>
+          <Headline size="large" faded><FormattedMessage id={`stripes-reshare.states.${request.state?.code}`} /></Headline>
+        </KeyValue>
+        <div className={css.cards}>
+          {renderNamedWithProps(forCurrent.cards, cards, { request })}
+        </div>
+        <Layout className="padding-top-gutter">
+          {PrimaryAction && <PrimaryAction request={request} name={forCurrent.primaryAction} performAction={performAction} /> }
+        </Layout>
+        {forCurrent.moreActions.length > 0 &&
+          <Layout className={`padding-top-gutter ${css.optionList} ${css.noBorderRadius}`}>
+            <strong>More options:</strong>
+            {renderNamedWithProps(forCurrent.moreActions, moreActions, { request, performAction }, moreActions.Generic)}
           </Layout>
-        </React.Fragment>
-      )}
-    </AppNameContext.Consumer>
+        }
+      </Layout>
+    </React.Fragment>
   );
 };
 
 FlowRoute.propTypes = {
-  stripes: PropTypes.shape({
-    hasPerm: PropTypes.func.isRequired,
-  }).isRequired,
   request: PropTypes.object.isRequired,
   performAction: PropTypes.func.isRequired,
 };
 
-export default withStripes(FlowRoute);
+export default FlowRoute;
