a1d3a43a046b526343ca01495ba472510b0a37bc
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-08-26 14:35:47 +0100

    put the actions into an accordion

--- src/components/Flow/FlowViewComponents/ActionAccordion.js
+++ src/components/Flow/FlowViewComponents/ActionAccordion.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
-import { Layout } from '@folio/stripes/components';
+import { Layout, Accordion } from '@folio/stripes/components';
 import { useStripes } from '@folio/stripes/core';
 import initialToUpper from '../../../util/initialToUpper';
 import renderNamedWithProps from '../../../util/renderNamedWithProps';
@@ -10,7 +10,7 @@ import * as moreActions from '../moreActions';
 import css from './Flow.css';
 import AppNameContext from '../../../AppNameContext';
 
-const ActionAccordion = ({ forCurrent, request, performAction }) => {
+const ActionAccordion = ({ forCurrent, id, onToggle, open, request, performAction }) => {
   const stripes = useStripes();
   const appName = useContext(AppNameContext);
 
@@ -20,25 +20,36 @@ const ActionAccordion = ({ forCurrent, request, performAction }) => {
       || primaryActions.Generic;
   }
   return (
-    stripes.hasPerm(`ui-${appName}.edit`) &&
-      <>
-        {PrimaryAction &&
-          <Layout className="padding-top-gutter">
-            <PrimaryAction request={request} name={forCurrent.primaryAction} performAction={performAction} />
-          </Layout>
-        }
-        {forCurrent.moreActions.length > 0 &&
-          <Layout className={`padding-top-gutter ${css.optionList} ${css.noBorderRadius}`}>
-            <strong>More options:</strong>
-            {renderNamedWithProps(forCurrent.moreActions, moreActions, { request, performAction }, moreActions.Generic)}
-          </Layout>
-        }
-      </>
+    <Accordion
+      id={id}
+      label="TESTING"
+      onToggle={onToggle}
+      open={open}
+    >
+      {stripes.hasPerm(`ui-${appName}.edit`) &&
+        <>
+          {PrimaryAction &&
+            <Layout className="padding-top-gutter">
+              <PrimaryAction request={request} name={forCurrent.primaryAction} performAction={performAction} />
+            </Layout>
+          }
+          {forCurrent.moreActions.length > 0 &&
+            <Layout className={`padding-top-gutter ${css.optionList} ${css.noBorderRadius}`}>
+              <strong>More options:</strong>
+              {renderNamedWithProps(forCurrent.moreActions, moreActions, { request, performAction }, moreActions.Generic)}
+            </Layout>
+          }
+        </>
+      }
+    </Accordion>
   );
 };
 
 ActionAccordion.propTypes = {
+  id: PropTypes.string.isRequired,
   forCurrent: PropTypes.object.isRequired,
+  onToggle: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
   request: PropTypes.object.isRequired,
   performAction: PropTypes.func.isRequired,
 };
--- src/routes/FlowRoute.js
+++ src/routes/FlowRoute.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import _ from 'lodash';
-import { Headline, KeyValue, Layout } from '@folio/stripes/components';
+import { Headline, KeyValue, Layout, AccordionSet } from '@folio/stripes/components';
 import renderNamedWithProps from '../util/renderNamedWithProps';
 import { actionsForRequest } from '../components/Flow/actionsByState';
 import * as cards from '../components/Flow/cardsByRequest';
@@ -10,22 +10,43 @@ import css from '../components/Flow/FlowViewComponents/Flow.css';
 import { ActionAccordion } from '../components/Flow/FlowViewComponents';
 
 const FlowRoute = ({ request, performAction }) => {
-  const forCurrent = actionsForRequest(request);
-  return (
+  const defaultAccordionState = {
+    actions: true
+  };
 
-    <Layout className="centered" style={{ maxWidth: '80em' }}>
-      <KeyValue label={<FormattedMessage id="stripes-reshare.requestState" />}>
-        <Headline size="large" faded><FormattedMessage id={`stripes-reshare.states.${request.state?.code}`} /></Headline>
-      </KeyValue>
-      <div className={css.cards}>
-        {renderNamedWithProps(forCurrent.cards, cards, { request })}
-      </div>
-      <ActionAccordion
-        forCurrent={forCurrent}
-        performAction={performAction}
-        request={request}
-      />
-    </Layout>
+  const forCurrent = actionsForRequest(request);
+  const [sections, setSections] = useState(defaultAccordionState);
+  const handleSectionToggle = ({ id }) => {
+    setSections((prevSections) => ({
+      sections: {
+        ...prevSections,
+        [id]: !prevSections[id],
+      }
+    }));
+  };
+
+  const getSectionProps = (id) => {
+    return {
+      forCurrent,
+      id,
+      onToggle: handleSectionToggle,
+      open: sections[id],
+      performAction,
+      request,
+    };
+  };
+  return (
+    <AccordionSet>
+      <Layout className="centered" style={{ maxWidth: '80em' }}>
+        <KeyValue label={<FormattedMessage id="stripes-reshare.requestState" />}>
+          <Headline size="large" faded><FormattedMessage id={`stripes-reshare.states.${request.state?.code}`} /></Headline>
+        </KeyValue>
+        <div className={css.cards}>
+          {renderNamedWithProps(forCurrent.cards, cards, { request })}
+        </div>
+        <ActionAccordion {...getSectionProps('actions')} />
+      </Layout>
+    </AccordionSet>
   );
 };
 
