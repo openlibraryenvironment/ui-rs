4df5ee1211145070e7bb00082b24fd5bcc74e2f1
Jason Skomorowski <jason@indexdata.com>
2021-01-05 15:15:37 -0500

    Simplify PatronRequestForm
    Accordion state no longer needs to be explicitly tracked and while the
    Expand All button can (iirc) work with the internal state, I don't think
    it's needed for this situation where we only have two collapsible
    sections.

--- src/components/PatronRequestForm/PatronRequestForm.js
+++ src/components/PatronRequestForm/PatronRequestForm.js
@@ -1,65 +1,165 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Field } from 'react-final-form';
+import { AccordionSet, Accordion, Col, Row, Datepicker, Select, TextField } from '@folio/stripes/components';
+import { required } from '@folio/stripes/util';
 
-import {
-  AccordionSet,
-  Col,
-  ExpandAllButton,
-  Row
-} from '@folio/stripes/components';
-
-import {
-  RequestMetadataForm,
-  PatronRequestInfoForm,
-} from './sections';
-
-class PatronRequestForm extends React.Component {
-  state = {
-    sections: {
-      requestMetadataForm: true,
-      patronRequestInfoForm: true,
-    }
-  }
-
-  getSectionProps() {
-    return {
-      onToggle: this.handleSectionToggle,
-    };
-  }
-
-  handleSectionToggle = ({ id }) => {
-    this.setState((prevState) => ({
-      sections: {
-        ...prevState.sections,
-        [id]: !prevState.sections[id],
-      }
-    }));
-  }
-
-  handleAllSectionsToggle = (sections) => {
-    this.setState({ sections });
-  }
-
-  render() {
-    const sectionProps = this.getSectionProps();
-    const { sections } = this.state;
-
-    return (
-      <div>
-        <AccordionSet>
-          <Row end="xs">
-            <Col xs>
-              <ExpandAllButton
-                accordionStatus={sections}
-                onToggle={this.handleAllSectionsToggle}
+const PatronRequestForm = () => (
+  <AccordionSet>
+    <Accordion label={<FormattedMessage id="ui-rs.information.heading.request" />}>
+      <Row>
+        <Col xs={3}>
+          <FormattedMessage id="ui-rs.information.hrid">
+            {(placeholder) => (
+              <Field
+                id="edit-request-metadata-hrid"
+                name="hrid"
+                label={placeholder}
+                component={TextField}
+                placeholder={placeholder}
               />
-            </Col>
-          </Row>
-          <RequestMetadataForm id="requestMetadataForm" open={sections.requestMetadataForm} {...sectionProps} />
-          <PatronRequestInfoForm id="patronRequestInfoForm" open={sections.patronRequestInfoForm} {...sectionProps} />
-        </AccordionSet>
-      </div>
-    );
-  }
-}
+            )}
+          </FormattedMessage>
+        </Col>
+        <Col xs={9}>
+          <FormattedMessage id="ui-rs.information.fullId">
+            {(placeholder) => (
+              <Field
+                id="edit-request-metadata-fullId"
+                name="id"
+                label={placeholder}
+                component={TextField}
+                placeholder={placeholder}
+                disabled
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6}>
+          <FormattedMessage id="ui-rs.information.requestingUser">
+            {(placeholder) => (
+              <Field
+                id="edit-request-metadata-requestingUser"
+                name="patronIdentifier"
+                label={placeholder}
+                component={TextField}
+                placeholder={placeholder}
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+        <Col xs={3}>
+          <FormattedMessage id="ui-rs.information.dateSubmitted">
+            {(placeholder) => (
+              <Field
+                id="edit-request-metadata-dateSubmitted"
+                name="formattedDateCreated"
+                label={placeholder}
+                component={TextField}
+                placeholder={placeholder}
+                disabled
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+        <Col xs={3}>
+          <FormattedMessage id="ui-rs.information.dateNeeded">
+            {(placeholder) => (
+              <Field
+                id="edit-request-metadata-neededBy"
+                name="neededBy"
+                dateFormat="YYYY-MM-DD"
+                backendDateStandard="YYYY-MM-DD"
+                label={placeholder}
+                component={Datepicker}
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6}>
+          <FormattedMessage id="ui-rs.information.pickupLocation">
+            {(placeholder) => (
+              <Field
+                id="edit-request-metadata-pickupLocation"
+                name="pickupLocation"
+                label={placeholder}
+                component={TextField}
+                placeholder={placeholder}
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+        <Col xs={6}>
+          <FormattedMessage id="ui-rs.information.notes">
+            {(placeholder) => (
+              <Field
+                id="edit-request-metadata-patronNote"
+                name="patronNote"
+                label={placeholder}
+                component={TextField}
+                placeholder={placeholder}
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+      </Row>
+    </Accordion>
+    <Accordion label={<FormattedMessage id="ui-rs.information.heading.requestedItem" />}>
+      <Row>
+        <Col xs={12}>
+          <FormattedMessage id="ui-rs.information.title">
+            {(placeholder) => (
+              <Field
+                id="edit-patron-request-title"
+                name="title"
+                label={placeholder}
+                component={TextField}
+                placeholder={placeholder}
+                required
+                validate={required}
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={3}>
+          <FormattedMessage id="ui-rs.information.date">
+            {(placeholder) => (
+              <Field
+                id="edit-patron-request-publicationDate"
+                name="publicationDate"
+                label={placeholder}
+                component={TextField}
+                placeholder={placeholder}
+                required
+                validate={required}
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+        <Col xs={9}>
+          <FormattedMessage id="ui-rs.information.author">
+            {(placeholder) => (
+              <Field
+                id="edit-patron-request-author"
+                name="author"
+                label={placeholder}
+                component={TextField}
+                placeholder={placeholder}
+                required
+                validate={required}
+              />
+            )}
+          </FormattedMessage>
+        </Col>
+      </Row>
+    </Accordion>
+  </AccordionSet>
+);
 
 export default PatronRequestForm;
