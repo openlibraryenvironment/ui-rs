d293f616fc62f86472d60036f193bfc00f26faba
Jason Skomorowski <jason@indexdata.com>
2021-04-06 08:38:42 -0400

    Forgot to prefix the prefix

--- src/components/Flow/modals/index.js
+++ src/components/Flow/modals/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ActionReasonModal from '../ActionReasonModal';
 
-export const SupplierCannotSupply = props => <ActionReasonModal action="supplierCannotSupply" reasonVocab="cannotSupplyReasons" reasonTranslationPrefix="settings.customiseListSelect.cannotSupplyReasons" {...props} />;
+export const SupplierCannotSupply = props => <ActionReasonModal action="supplierCannotSupply" reasonVocab="cannotSupplyReasons" reasonTranslationPrefix="ui-rs.settings.customiseListSelect.cannotSupplyReasons" {...props} />;
 export const RequesterCancel = props => <ActionReasonModal action="requesterCancel" reasonVocab="cancellationReasons" {...props} />;
-export const CancelLocal = props => <ActionReasonModal action="cancelLocal" reasonVocab="cancellationReasons" reasonTranslationPrefix="actions.requesterCancel.reasons" {...props} />;
+export const CancelLocal = props => <ActionReasonModal action="cancelLocal" reasonVocab="cancellationReasons" reasonTranslationPrefix="ui-rs.actions.requesterCancel.reasons" {...props} />;
 
 export const Cancel = () => null;
 export { default as RespondYes } from './RespondYes';
