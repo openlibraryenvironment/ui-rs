93c7d39b7596695a917f72891ec996679ea5caf0
Jason Skomorowski <jason@indexdata.com>
2021-04-12 16:56:56 -0400

    Do not allow deletion of cancellation reasons

--- src/settings/OtherSettings.js
+++ src/settings/OtherSettings.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { useIntl } from 'react-intl';
 import ControlledRefdata from './settingsComponents/ControlledRefdata';
 
+const actionSuppressor = {
+  edit: item => item.readOnly,
+  delete: () => true,
+};
+
 const OtherSettings = () => {
   const intl = useIntl();
   return (<ControlledRefdata
     category="cancellationReasons"
     label={intl.formatMessage({ id: 'ui-rs.settingName.cancellationReasons' })}
     formatter={{ label: r => intl.formatMessage({ id: `ui-rs.actions.requesterCancel.reasons.${r.value}`, defaultMessage: r.label }) }}
+    actionSuppressor={actionSuppressor}
   />);
 };
 
