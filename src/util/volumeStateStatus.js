0eb243ba4e1504fbfccd28a2fad4cc005d20b849
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-06-03 13:07:31 +0100

    tweaks to volumeStateStatus function, translations

--- src/util/volumeStateStatus.js
+++ src/util/volumeStateStatus.js
@@ -3,14 +3,17 @@
   * from the request state code, whether that volume is in an acceptable state or not
   */
 const volumeStateStatus = (vol, code) => {
-  if (code === 'RES_AWAIT_PICKING' ||
-      code === 'RES_AWAIT_PROXY_BORROWER' ||
-      code === 'RES_AWAIT_SHIP'
-  ) {
-    // At this point, the ideal scenario is 'LMS check out complete'
-    return vol.status?.value === 'lms_check_out_complete';
+  switch (code) {
+    case (code.match(/^RES/) || {}).input:
+      const volValue = vol.status?.value
+      // If on the supplier's side, check not in an awaiting state
+      return volValue === 'lms_check_out_complete' || volValue === 'completed';
+    case (code.match(/^REQ/) || {}).input:
+      // If on the requester's side, just need to know if temp item created or not
+      return vol.status?.value === 'temporary_item_created_in_host_lms'
+    default:
+      return false;
   }
-  return false;
 };
 
 export default volumeStateStatus;
