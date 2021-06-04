b9832a0c67d87b471d9d2aa8391d025da1a7468b
Mike Taylor <mike@indexdata.com>
2021-06-04 17:59:47 +0100

    Omit malformed dates from filters
    When the filterstring contains elements beginning with 'date' and not
    ending with a well-formed ISO date, those elements are simply
    discarded from the generated query.
    
    I had hoped this would fix PR-1002, and it does indeed ensure that the
    query sent is always correct and never results in an error-message
    from the back-end module.
    
    But that's not good enough, because typing into a date-filter box
    still causes a re-render on each character, which defocuses the
    date-filter box so that you need to click back into it for each
    character.

--- src/routes/PatronRequestsRoute.js
+++ src/routes/PatronRequestsRoute.js
@@ -57,9 +57,20 @@ const appDetails = {
   },
 };
 
+const removeMalformedDates = (filterString) => {
+  return filterString && filterString
+    .split(',')
+    .filter(kv => {
+      return (!kv.startsWith('date') ||
+              kv.match(/[<>=]\d{4}-\d{2}-\d{2}/));
+    })
+    .join(',');
+};
+
 function queryModifiedForApp(resources, props) {
   const { appName } = props;
   const res = { ...resources.query };
+  res.filters = removeMalformedDates(res.filters);
   const { extraFilter } = appDetails[appName];
   if (extraFilter) {
     res.filters = !res.filters ? extraFilter : `${res.filters},${extraFilter}`;
