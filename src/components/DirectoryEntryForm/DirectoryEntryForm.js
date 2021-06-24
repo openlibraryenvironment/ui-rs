bbccd7e09ac8e00c7d1d020d7e10cbd2133d6db4
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2021-06-24 16:11:22 +0100

    Refactored code to functional, fixed issue where new managed entries were not fully editable (#11)

--- src/components/DirectoryEntryForm/DirectoryEntryForm.js
+++ src/components/DirectoryEntryForm/DirectoryEntryForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
@@ -24,165 +24,149 @@ import {
   LocalDirectoryEntryFormInfo,
 } from './sectionsLocal';
 
-class DirectoryEntryForm extends React.Component {
-  static propTypes = {
-    form: PropTypes.object,
-    parentResources: PropTypes.object,
-    resources: PropTypes.shape({
-      selectedRecord: PropTypes.shape({
-        records: PropTypes.array,
-      }),
-    }),
-    values: PropTypes.object,
-  }
+const DirectoryEntryForm = ({
+  form,
+  initialValues,
+  parentResources,
+  resources,
+  stripes,
+}) => {
+  const layer = resources?.query?.layer ?? parentResources?.query?.layer;
+  const managed = initialValues?.status?.value === 'managed' ||
+    layer === 'create' ||
+    layer === 'unit';
 
-  constructor(props) {
-    super(props);
+  const localOnly = (stripes.hasPerm('ui-directory.edit-local') &&
+                    !stripes.hasPerm('ui-directory.edit-all') &&
+                    !(managed && stripes.hasPerm('ui-directory.edit-self')));
 
-    const { stripes } = props;
-    const managed = props.initialValues?.status?.value === 'managed';
-    this.localOnly = (stripes.hasPerm('ui-directory.edit-local') &&
-                      !stripes.hasPerm('ui-directory.edit-all') &&
-                      !(managed && stripes.hasPerm('ui-directory.edit-self')));
-    /*
-    console.log(`edit-local=${stripes.hasPerm('ui-directory.edit-local')}, ` +
-                `edit-all=${stripes.hasPerm('ui-directory.edit-all')}, ` +
-                `edit-self=${stripes.hasPerm('ui-directory.edit-self')}, ` +
-                `managed=${managed} --> ` +
-                `localOnly=${this.localOnly}`);
-    */
 
-    this.state = {
-      sectionsShared: {
-        directoryEntryFormInfo: true,
-        directoryEntryFormContactInfo: true,
-        directoryEntryFormServices: false,
-        directoryEntryFormCustProps: false,
-      },
-      sectionsLocal: {
-        localDirectoryEntryFormInfo: this.localOnly,
-      },
-      tab: this.localOnly ? 'local' : 'shared',
-    };
-  }
+  const [sectionsShared, setSectionsShared] = useState({
+    directoryEntryFormInfo: true,
+    directoryEntryFormContactInfo: true,
+    directoryEntryFormServices: false,
+    directoryEntryFormCustProps: false,
+  });
 
-  getSectionProps() {
-    const { form } = this.props;
-    return {
-      form,
-      onToggle: this.handleSectionToggle,
-      parentResources: this.props.parentResources,
-    };
-  }
+  const [sectionsLocal, setSectionsLocal] = useState({
+    localDirectoryEntryFormInfo: localOnly,
+  });
 
-  handleSectionToggle = ({ id }) => {
-    this.setState((prevState) => ({
-      sectionsShared: {
-        ...prevState.sectionsShared,
-        [id]: !prevState.sectionsShared[id],
-      },
-      sectionsLocal: {
-        ...prevState.sectionsLocal,
-        [id]: !prevState.sectionsLocal[id],
-      }
-    }));
-  }
+  const [tab, setTab] = useState(localOnly ? 'local' : 'shared');
 
-  handleAllSectionsToggleShared = (sectionsShared) => {
-    this.setState({ sectionsShared });
-  }
 
-  handleAllSectionsToggleLocal = (sectionsLocal) => {
-    this.setState({ sectionsLocal });
-  }
-
-  render() {
-    const sectionProps = this.getSectionProps();
-    const { sectionsShared, sectionsLocal, tab } = this.state;
-
-    const selectedRecord = this.props.resources ? this.props.resources.selectedRecord : {};
-    let name;
-    if (selectedRecord) {
-      if (selectedRecord.records) {
-        if (selectedRecord.records[0]) {
-          name = selectedRecord.records[0].fullyQualifiedName;
-        }
-      }
-    } else {
-      name = 'this institution';
+  const handleSectionToggle = ({ id }) => {
+    if (Object.prototype.hasOwnProperty.call(sectionsShared, id)) {
+      const sections = { ...sectionsShared };
+      sections[id] = !sectionsShared[id];
+      setSectionsShared(sections);
     }
-    return (
-      <div>
-        <Layout className="textCentered">
-          <ButtonGroup>
-            {!this.localOnly &&
-              <Button
-                onClick={() => this.setState({ tab: 'shared' })}
-                buttonStyle={tab === 'shared' ? 'primary' : 'default'}
-                id="clickable-nav-shared"
-              >
-                <FormattedMessage id="ui-directory.information.tab.shared" />
-              </Button>
-            }
+
+    if (Object.prototype.hasOwnProperty.call(sectionsLocal, id)) {
+      const sections = { ...sectionsLocal };
+      sections[id] = !sectionsLocal[id];
+      setSectionsLocal(sections);
+    }
+  };
+
+  const handleAllSectionsToggleShared = (sections) => {
+    setSectionsShared(sections);
+  };
+
+  const handleAllSectionsToggleLocal = (sections) => {
+    setSectionsLocal(sections);
+  };
+
+  const sectionProps = {
+    form,
+    onToggle: handleSectionToggle,
+    parentResources,
+  };
+
+  const name = resources?.selectedRecord?.records?.[0]?.fullyQualifiedName ??
+    'this institution';
+  return (
+    <div>
+      <Layout className="textCentered">
+        <ButtonGroup>
+          {!localOnly &&
             <Button
-              onClick={() => this.setState({ tab: 'local' })}
-              buttonStyle={tab === 'local' ? 'primary' : 'default'}
-              id="clickable-nav-local"
+              onClick={() => setTab('shared')}
+              buttonStyle={tab === 'shared' ? 'primary' : 'default'}
+              id="clickable-nav-shared"
             >
-              <FormattedMessage id="ui-directory.information.tab.local" />
+              <FormattedMessage id="ui-directory.information.tab.shared" />
             </Button>
-          </ButtonGroup>
-        </Layout>
-        {tab === 'shared' &&
-          <>
-            <Row>
-              <Col xs={12} lgOffset={1} lg={10}>
-                <MessageBanner>
-                  <FormattedMessage id="ui-directory.information.heading.display-text" values={{ directory_entry: name }} />
-                </MessageBanner>
+          }
+          <Button
+            onClick={() => setTab('local')}
+            buttonStyle={tab === 'local' ? 'primary' : 'default'}
+            id="clickable-nav-local"
+          >
+            <FormattedMessage id="ui-directory.information.tab.local" />
+          </Button>
+        </ButtonGroup>
+      </Layout>
+      {tab === 'shared' &&
+        <>
+          <Row>
+            <Col xs={12} lgOffset={1} lg={10}>
+              <MessageBanner>
+                <FormattedMessage id="ui-directory.information.heading.display-text" values={{ directory_entry: name }} />
+              </MessageBanner>
+            </Col>
+          </Row>
+          <AccordionSet>
+            <Row end="xs">
+              <Col xs>
+                <ExpandAllButton
+                  accordionStatus={sectionsShared}
+                  onToggle={handleAllSectionsToggleShared}
+                />
               </Col>
             </Row>
-            <AccordionSet>
-              <Row end="xs">
-                <Col xs>
-                  <ExpandAllButton
-                    accordionStatus={sectionsShared}
-                    onToggle={this.handleAllSectionsToggleShared}
-                  />
-                </Col>
-              </Row>
-              <DirectoryEntryFormInfo id="directoryEntryFormInfo" open={sectionsShared.directoryEntryFormInfo} {...sectionProps} />
-              <DirectoryEntryFormContactInfo id="directoryEntryFormContactInfo" open={sectionsShared.directoryEntryFormContactInfo} {...sectionProps} />
-              <DirectoryEntryFormServices id="directoryEntryFormServices" open={sectionsShared.directoryEntryFormServices} {...sectionProps} />
-              <DirectoryEntryFormCustProps id="directoryEntryFormCustProps" open={sectionsShared.directoryEntryFormCustProps} {...sectionProps} />
-            </AccordionSet>
-          </>
-        }
-        {tab === 'local' &&
-          <>
-            <Row>
-              <Col xs={12} lgOffset={1} lg={10}>
-                <MessageBanner>
-                  <FormattedMessage id="ui-directory.information.local.heading.display-text" />
-                </MessageBanner>
+            <DirectoryEntryFormInfo id="directoryEntryFormInfo" open={sectionsShared.directoryEntryFormInfo} {...sectionProps} />
+            <DirectoryEntryFormContactInfo id="directoryEntryFormContactInfo" open={sectionsShared.directoryEntryFormContactInfo} {...sectionProps} />
+            <DirectoryEntryFormServices id="directoryEntryFormServices" open={sectionsShared.directoryEntryFormServices} {...sectionProps} />
+            <DirectoryEntryFormCustProps id="directoryEntryFormCustProps" open={sectionsShared.directoryEntryFormCustProps} {...sectionProps} />
+          </AccordionSet>
+        </>
+      }
+      {tab === 'local' &&
+        <>
+          <Row>
+            <Col xs={12} lgOffset={1} lg={10}>
+              <MessageBanner>
+                <FormattedMessage id="ui-directory.information.local.heading.display-text" />
+              </MessageBanner>
+            </Col>
+          </Row>
+          <AccordionSet>
+            <Row end="xs">
+              <Col xs>
+                <ExpandAllButton
+                  accordionStatus={sectionsLocal}
+                  onToggle={handleAllSectionsToggleLocal}
+                />
               </Col>
             </Row>
-            <AccordionSet>
-              <Row end="xs">
-                <Col xs>
-                  <ExpandAllButton
-                    accordionStatus={sectionsLocal}
-                    onToggle={this.handleAllSectionsToggleLocal}
-                  />
-                </Col>
-              </Row>
-              <LocalDirectoryEntryFormInfo id="localDirectoryEntryFormInfo" open={sectionsLocal.localDirectoryEntryFormInfo} {...sectionProps} />
-            </AccordionSet>
-          </>
-        }
-      </div>
-    );
-  }
-}
+            <LocalDirectoryEntryFormInfo id="localDirectoryEntryFormInfo" open={sectionsLocal.localDirectoryEntryFormInfo} {...sectionProps} />
+          </AccordionSet>
+        </>
+      }
+    </div>
+  );
+};
+
+DirectoryEntryForm.propTypes = {
+  form: PropTypes.object,
+  parentResources: PropTypes.object,
+  resources: PropTypes.shape({
+    selectedRecord: PropTypes.shape({
+      records: PropTypes.array,
+    }),
+  }),
+  values: PropTypes.object,
+};
 
 export default DirectoryEntryForm;
--- src/components/EditDirectoryEntry/EditDirectoryEntry.js
+++ src/components/EditDirectoryEntry/EditDirectoryEntry.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage, injectIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { Form } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
 import { Prompt } from 'react-router-dom';
@@ -34,40 +34,22 @@ plugins.forEach(plugin => {
   });
 });
 
-class EditDirectoryEntry extends React.Component {
-  static propTypes = {
-    initialValues: PropTypes.object,
-    onSubmit: PropTypes.func,
-    onCancel: PropTypes.func,
-    onUpdate: PropTypes.func.isRequired,
-    parentResources:PropTypes.shape({
-      query: PropTypes.shape({
-        layer: PropTypes.string,
-      }),
-    }),
-    resources: PropTypes.shape({
-      query: PropTypes.shape({
-        layer: PropTypes.string,
-      }),
-    }),
-    stripes: PropTypes.shape({
-      hasPerm: PropTypes.func.isRequired,
-    }).isRequired,
-    intl: PropTypes.shape({
-      formatMessage: PropTypes.func.isRequired,
-    }).isRequired,
-  }
+const EditDirectoryEntry = (props) => {
+  // Destructure this separately because onSubmit appears to take the entirety of props atm...?
+  const {
+    initialValues,
+    onCancel,
+    onSubmit = defaultSubmit,
+    resources,
+    stripes,
+    parentResources
+  } = props;
 
-  static defaultProps = {
-    onSubmit: defaultSubmit
-  }
+  const intl = useIntl();
 
-  getCurrentLayer() {
-    const layer = this.props?.resources ? this.props?.resources?.query?.layer : this.props?.parentResources?.query?.layer;
-    return layer;
-  }
+  const layer = resources?.query?.layer ?? parentResources?.query?.layer;
 
-  renderFirstMenu() {
+  const renderFirstMenu = () => {
     return (
       <PaneMenu>
         <FormattedMessage id="ui-directory.closeNewDirectoryEntry">
@@ -75,20 +57,18 @@ class EditDirectoryEntry extends React.Component {
             <IconButton
               icon="times"
               id="close-directory-form-button"
-              onClick={this.props.onCancel}
+              onClick={onCancel}
               aria-label={ariaLabel}
             />
           )}
         </FormattedMessage>
       </PaneMenu>
     );
-  }
+  };
 
-  renderLastMenu(pristine, submitting, submit) {
+  const renderLastMenu = (pristine, submitting, submit) => {
     let id;
     let label;
-    const layer = this.getCurrentLayer();
-
     if (layer === 'edit') {
       id = 'clickable-update-directory-entry';
       label = <FormattedMessage id="ui-directory.updateDirectoryEntryNoName" />;
@@ -111,143 +91,163 @@ class EditDirectoryEntry extends React.Component {
         </Button>
       </PaneMenu>
     );
-  }
+  };
 
-  selectPlugin(domain) {
-    const { intl } = this.props;
-    const plugin = domain ? (pluginMap[domain] ? pluginMap[domain] : pluginMap.Generic) : undefined;
+  const selectPlugin = (domain) => {
+    if (!domain) {
+      return undefined;
+    }
+
+    const plugin = pluginMap[domain] ?? pluginMap.Generic;
 
     if (!plugin) {
       throw new Error(intl.formatMessage({ id: 'ui-directory.information.addresses.missingPlugin' }));
     }
     return plugin;
+  };
+
+  let paneTitle = <FormattedMessage id="ui-directory.notSet" />;
+  switch (layer) {
+    case 'edit':
+      if (initialValues && initialValues.id) {
+        paneTitle = <FormattedMessage id="ui-directory.updateDirectoryEntry" values={{ dirent: initialValues.name }} />;
+      } else {
+        paneTitle = <FormattedMessage id="ui-directory.updateDirectoryEntryNoName" />;
+      }
+      break;
+    case 'unit':
+      paneTitle = <FormattedMessage id="ui-directory.createUnitDirectoryEntry" />;
+      break;
+    case 'create':
+      paneTitle = <FormattedMessage id="ui-directory.createDirectoryEntry" />;
+      break;
+    default:
+      break;
   }
 
-  render() {
-    const { initialValues, onSubmit, stripes } = this.props;
+  if (!permissionToEdit(stripes, initialValues) && !stripes.hasPerm('ui-directory.create')) {
+    // Users should never see this message, so no need to internationalize
+    return 'no perm';
+  }
 
-    // Grab current layer
-    const layer = this.getCurrentLayer();
-    let paneTitle = <FormattedMessage id="ui-directory.notSet" />;
-    switch (layer) {
-      case 'edit':
-        if (initialValues && initialValues.id) {
-          paneTitle = <FormattedMessage id="ui-directory.updateDirectoryEntry" values={{ dirent: initialValues.name }} />;
+
+  // TODO this seems like terrible code...
+  // This allows the initial values to hold the current parent value
+  if (initialValues) {
+    initialValues.parent = initialValues.parent?.id;
+    initialValues.type = initialValues.type?.id;
+
+    if (initialValues.services) {
+      const newServices = [];
+      initialValues.services.forEach(serviceacct => {
+        newServices.push({ ...serviceacct, service: serviceacct.service.id });
+      });
+      initialValues.services = newServices;
+    }
+  }
+
+  // the submit handler passed in from SearchAndSort expects props as provided by redux-form
+  const compatSubmit = values => {
+    // Not submitting values itself because then on failure data changes shape
+    const submitValues = { ...values };
+
+    // When creating a NEW entry we want to set status to managed, but when editing an existing one we want to leave alone
+    if (layer === 'create' || layer === 'unit') {
+      // When creating a root or new entry, the layer is "create" or "unit", else layer is "edit"
+      const managedStatus = getRefdataValuesFromParentResources(parentResources, 'DirectoryEntry.Status')
+        .filter(obj => obj.label === 'Managed')[0] || {};
+      submitValues.status = managedStatus.value;
+    }
+
+    if (values.parent) {
+      submitValues.parent = { id: values.parent };
+    }
+
+    if (values.services) {
+      const newServices = [];
+      values.services.forEach(serviceacct => {
+        newServices.push({ ...serviceacct, service: { id: serviceacct.service } });
+      });
+      submitValues.services = newServices;
+    }
+
+    submitValues.symbols = values.symbols?.map(obj => (obj?.authority?.id ? obj : ({ ...obj, authority: { id: obj.authority } })));
+
+    if (submitValues.addresses) {
+      const newAddresses = [];
+      submitValues.addresses.forEach((address) => {
+        if (address._delete === true) {
+          // If we're deleting the address we can just leave it as is
+          newAddresses.push(address);
         } else {
-          paneTitle = <FormattedMessage id="ui-directory.updateDirectoryEntryNoName" />;
+          const plugin = selectPlugin(address.countryCode);
+          const newAddress = plugin.fieldsToBackend(address);
+          // The plugins do not have the notion of seq
+          // so we obtain their fieldOrder and add that manually
+          const fieldOrder = plugin.fieldOrder;
+          newAddress.lines.forEach(line => {
+            const lineType = line.type?.value;
+            line.seq = fieldOrder[lineType];
+          });
+          newAddresses.push(newAddress);
         }
-        break;
-      case 'unit':
-        paneTitle = <FormattedMessage id="ui-directory.createUnitDirectoryEntry" />;
-        break;
-      case 'create':
-        paneTitle = <FormattedMessage id="ui-directory.createDirectoryEntry" />;
-        break;
-      default:
-        break;
+      });
+      submitValues.addresses = newAddresses;
     }
+    onSubmit(submitValues, null, props);
+  };
 
-    if (!permissionToEdit(stripes, initialValues) && !stripes.hasPerm('ui-directory.create')) {
-      // Users should never see this message, so no need to internationalize
-      return 'no perm';
-    }
+  return (
+    <Form
+      onSubmit={compatSubmit}
+      initialValues={initialValues}
+      keepDirtyOnReinitialize
+      mutators={{
+        ...arrayMutators,
+      }}
+    >
+      {({ form, handleSubmit, pristine, submitting, submitSucceeded, values }) => (
+        <form id="form-directory-entry">
+          <Pane
+            defaultWidth="100%"
+            firstMenu={renderFirstMenu()}
+            lastMenu={renderLastMenu(pristine, submitting, handleSubmit)}
+            paneTitle={paneTitle}
+          >
+            <Layout className="centered" style={{ maxWidth: '80em' }}>
+              <DirectoryEntryForm values={values} form={form} {...props} />
+              <FormattedMessage id="ui-directory.confirmDirtyNavigate">
+                {prompt => <Prompt when={!pristine && !(submitting || submitSucceeded)} message={prompt} />}
+              </FormattedMessage>
+            </Layout>
+          </Pane>
+        </form>
+      )}
+    </Form>
+  );
+};
 
-    // This allows the initial values to hold the current parent value
-    if (initialValues) {
-      if (initialValues.parent) {
-        initialValues.parent = initialValues.parent.id;
-      }
-      if (initialValues.type) {
-        initialValues.type = initialValues.type.id;
-      }
-      if (initialValues.services) {
-        const newServices = [];
-        initialValues.services.forEach(serviceacct => {
-          newServices.push({ ...serviceacct, service: serviceacct.service.id });
-        });
-        initialValues.services = newServices;
-      }
-    }
-    // the submit handler passed in from SearchAndSort expects props as provided by redux-form
-    const compatSubmit = values => {
-      // Not submitting values itself because then on failure data changes shape
-      const submitValues = { ...values };
+EditDirectoryEntry.propTypes = {
+  initialValues: PropTypes.object,
+  onSubmit: PropTypes.func,
+  onCancel: PropTypes.func,
+  onUpdate: PropTypes.func.isRequired,
+  parentResources:PropTypes.shape({
+    query: PropTypes.shape({
+      layer: PropTypes.string,
+    }),
+  }),
+  resources: PropTypes.shape({
+    query: PropTypes.shape({
+      layer: PropTypes.string,
+    }),
+  }),
+  stripes: PropTypes.shape({
+    hasPerm: PropTypes.func.isRequired,
+  }).isRequired,
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
-      // When creating a NEW entry we want to set status to managed, but when editing an existing one we want to leave alone
-      if (layer === 'create' || layer === 'unit') {
-        // When creating a root or new entry, the layer is "create" or "unit", else layer is "edit"
-        const managedStatus = getRefdataValuesFromParentResources(this.props.parentResources, 'DirectoryEntry.Status')
-          .filter(obj => obj.label === 'Managed')[0] || {};
-        submitValues.status = managedStatus.value;
-      }
-
-      if (values.parent) {
-        submitValues.parent = { id: values.parent };
-      }
-
-      if (values.services) {
-        const newServices = [];
-        values.services.forEach(serviceacct => {
-          newServices.push({ ...serviceacct, service: { id: serviceacct.service } });
-        });
-        submitValues.services = newServices;
-      }
-
-      submitValues.symbols = values.symbols?.map(obj => (obj?.authority?.id ? obj : ({ ...obj, authority: { id: obj.authority } })));
-
-      if (submitValues.addresses) {
-        const newAddresses = [];
-        submitValues.addresses.forEach((address) => {
-          if (address._delete === true) {
-            // If we're deleting the address we can just leave it as is
-            newAddresses.push(address);
-          } else {
-            const plugin = this.selectPlugin(address.countryCode);
-            const newAddress = plugin.fieldsToBackend(address);
-            // The plugins do not have the notion of seq
-            // so we obtain their fieldOrder and add that manually
-            const fieldOrder = plugin.fieldOrder;
-            newAddress.lines.forEach(line => {
-              const lineType = line.type?.value;
-              line.seq = fieldOrder[lineType];
-            });
-            newAddresses.push(newAddress);
-          }
-        });
-        submitValues.addresses = newAddresses;
-      }
-      onSubmit(submitValues, null, this.props);
-    };
-
-    return (
-      <Form
-        onSubmit={compatSubmit}
-        initialValues={initialValues}
-        keepDirtyOnReinitialize
-        mutators={{
-          ...arrayMutators,
-        }}
-      >
-        {({ form, handleSubmit, pristine, submitting, submitSucceeded, values }) => (
-          <form id="form-directory-entry">
-            <Pane
-              defaultWidth="100%"
-              firstMenu={this.renderFirstMenu()}
-              lastMenu={this.renderLastMenu(pristine, submitting, handleSubmit)}
-              paneTitle={paneTitle}
-            >
-              <Layout className="centered" style={{ maxWidth: '80em' }}>
-                <DirectoryEntryForm values={values} form={form} {...this.props} />
-                <FormattedMessage id="ui-directory.confirmDirtyNavigate">
-                  {prompt => <Prompt when={!pristine && !(submitting || submitSucceeded)} message={prompt} />}
-                </FormattedMessage>
-              </Layout>
-            </Pane>
-          </form>
-        )}
-      </Form>
-    );
-  }
-}
-
-export default injectIntl(EditDirectoryEntry);
+export default EditDirectoryEntry;
