71deca6a8266af429dbdb7b0cb6133ca337a1fd4
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2021-07-22 17:23:58 +0100

    PR-751, PR-979 (#59)
    * changes to FillMultiVolumeRequest modal, now no default volume name
    
    * fixes for PR-979
    
    * linting

--- src/util/volumeStateStatus.js
+++ src/util/volumeStateStatus.js
@@ -5,12 +5,12 @@
 const volumeStateStatus = (vol, code) => {
   switch (code) {
     case (code.match(/^RES/) || {}).input:
-      const volValue = vol.status?.value
+      const volValue = vol.status?.value;
       // If on the supplier's side, check not in an awaiting state
       return volValue === 'lms_check_out_complete' || volValue === 'completed';
     case (code.match(/^REQ/) || {}).input:
       // If on the requester's side, just need to know if temp item created or not
-      return vol.status?.value === 'temporary_item_created_in_host_lms'
+      return vol.status?.value === 'temporary_item_created_in_host_lms';
     default:
       return false;
   }
