bb28c4ffec9172db9db85fd788d50c408b433974
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-08-12 12:21:23 +0100

    Tweaked rendering of viewMessageBanners

--- src/components/ViewMessageBanners.js
+++ src/components/ViewMessageBanners.js
@@ -13,30 +13,40 @@ const ViewMessageBanners = ({ request }) => {
     condition => condition.relevantSupplier?.id === request.resolvedSupplier?.id && condition.accepted === true
   );
 
-  return (
-    <>
-      {request?.requesterRequestedCancellation ?
-        <MessageBanner
-          type="warning"
-        >
-          <FormattedMessage id="ui-rs.actions.requesterRequestedCancellation" />
-        </MessageBanner> : null
-      }
-      {relevantPendingConditions.length > 0 ?
+  const cancellationRequested = request?.state?.code === 'RES_CANCEL_REQUEST_RECEIVED';
+
+  const renderConditionsBanner = () => {
+    if (relevantPendingConditions.length > 0) {
+      return (
         <MessageBanner
           type="warning"
         >
           <SafeHTMLMessage id="ui-rs.actions.requestPendingLoanConditions" />
-        </MessageBanner> : null
-      }
-      { /* It's not particularly useful to show two banners when there is a pending AND accepted request */}
-      {relevantAcceptedConditions.length > 0 && relevantPendingConditions.length === 0 ?
+        </MessageBanner>
+      );
+    } else if (relevantAcceptedConditions.length > 0) {
+      /* It's not particularly useful to show two banners when there is a pending AND accepted request */
+      return (
         <MessageBanner
           type="success"
         >
           <SafeHTMLMessage id="ui-rs.actions.requestAcceptedLoanConditions" />
-        </MessageBanner> : null
+        </MessageBanner>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <>
+      {cancellationRequested &&
+        <MessageBanner
+          type="warning"
+        >
+          <FormattedMessage id="ui-rs.actions.requesterRequestedCancellation" />
+        </MessageBanner>
       }
+      {renderConditionsBanner()}
     </>
   );
 };
