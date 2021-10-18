ecb262cf73689a9353d2284aae3d9a4b35eb0def
Jason Skomorowski <jason@indexdata.com>
2021-10-18 18:13:40 -0400

    Let back end parse due date PR-1132

--- src/components/Flow/FlowViewComponents/RequestInfo.js
+++ src/components/Flow/FlowViewComponents/RequestInfo.js
@@ -6,18 +6,17 @@ import { Accordion, Col, Headline, KeyValue, Layout, NoValue, Row } from '@folio
 
 
 function calculateDueDate(intl, request) {
+  if (request.parsedDueDateRS) {
+    return intl.formatDate(request.parsedDueDateRS, {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+  }
   const dueDateText = request.dueDateFromLMS || request.dueDateRS;
   if (!dueDateText) return <NoValue />;
-
-  // By inspection (the documentation is useless) intl.formatDate does
-  // not throw an exception or return undefined when fed a bad date,
-  // but vomits all over the JavaScript console and returns the
-  // human-readable string "Invalid Date". That's no use, so we have
-  // to check by hand whether the date string is OK. _sigh_
-
-  if (dueDateText.match(/^\d{4}-\d{2}-\d{2}([T ]\d{2}:\d{2}:\d{2})?$/)) {
-    return <FormattedDate value={dueDateText} />;
-  }
   return <FormattedMessage id="ui-rs.invalid-date" values={{ date: dueDateText }} />;
 }
 
