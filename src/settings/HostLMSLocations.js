6cb8d438431ef92e8e6ec716c7e25179055f72c5
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-03-04 13:16:38 +0000

    refactor: Refactor HostLMSSettings
    Refactored hostLMSSettings component away from controlledVocab over to ActionList component from stripes-kint-components.
    
    PR-1259

--- src/settings/HostLMSLocations.js
+++ src/settings/HostLMSLocations.js
@@ -1,22 +1,66 @@
 import React from 'react';
-import { useIntl, FormattedMessage } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
+import { useMutation, useQuery } from 'react-query';
+
 import { Field } from 'react-final-form';
-import { Select } from '@folio/stripes/components';
-import { ControlledVocab } from '@folio/stripes/smart-components';
-import { stripesConnect, useStripes } from '@folio/stripes/core';
 
-const ConnectedControlledVocab = stripesConnect(ControlledVocab);
-const actionSuppressor = { edit: () => false, delete: () => true };
-const HostLMSLocations = ({ resources }) => {
-  const intl = useIntl();
-  const stripes = useStripes();
+import { Pane, Select, Spinner } from '@folio/stripes/components';
+import { useOkapiKy } from '@folio/stripes/core';
 
-  if (!resources?.branchLocations?.hasLoaded) return null;
-  const dirOptions = resources.branchLocations.records.reduce((acc, cur) => ([...acc, { value: cur.id, label: cur.name }]), []);
-  const dirLookup = dirOptions.reduce((acc, cur) => ({ ...acc, [cur.value]: cur.label }), {});
+import { ActionList, generateKiwtQuery } from '@k-int/stripes-kint-components';
+
+const HostLMSLocations = () => {
+  const ky = useOkapiKy();
+
+  const { data: locations } = useQuery(
+    ['ui-rs', 'settings', 'HostLMSLocations', 'getLocations'],
+    () => ky(`rs/hostLMSLocations${generateKiwtQuery({sort: 'name', stats: false, max: 100}, {})}`).json()
+  );
+
+  const branchPath = `rs/directoryEntry${generateKiwtQuery(
+    {
+      filterKeys: {
+        entryType: 'tpye.value',
+        entryStatus: 'status.value'
+      },
+      max: 100,
+      sort: 'name',
+      stats: false
+    },
+    {
+      filters: 'entryType.branch, entryStatus.managed'
+    }
+  )}`;
+
+  const { data: branchLocations, isLoading: branchLocationsLoading } = useQuery(
+    ['ui-rs', 'settings', 'HostLMSLocations', 'getBranchLocations'],
+    () => ky(branchPath).json()
+  );
+
+  const dirOptions = branchLocations?.reduce((acc, cur) => ([...acc, { value: cur.id, label: cur.name }]), [{"": ""}]);
+  // This is fine for now but we should probably just expand the dir entry name on the record
+  const dirLookup = dirOptions?.reduce((acc, cur) => ({ ...acc, [cur.value]: cur.label }), {});
+
+
+  const { mutateAsync: putLocation } = useMutation(
+    ['ui-rs', 'settings', 'HostLMSLocations', 'putLocations'],
+    async (data) => ky.put(`rs/hostLMSLocations/${data.id}`, { json: data }).json()
+  );
+
+  console.log("Locations: %o", locations)
+
+  const actionAssigner = () => {
+    return ([
+      { name: 'edit', label: <FormattedMessage id="ui-rs.edit" />, icon: 'edit' },
+    ]);
+  };
+
+  const actionCalls = {
+    edit: (data) => putLocation(data)
+  };
 
   const fieldComponents = {
-    correspondingDirectoryEntry: ({ fieldProps }) => {
+    correspondingDirectoryEntry: ({ ...fieldProps }) => {
       return (
         <Field
           {...fieldProps}
@@ -31,43 +75,39 @@ const HostLMSLocations = ({ resources }) => {
     },
   };
 
-  const formatter = {
-    correspondingDirectoryEntry: rec => {
-      const id = rec?.correspondingDirectoryEntry?.id;
-      if (!id) return '';
-      return dirLookup[id] ?? id;
-    }
-  };
 
-  return (<ConnectedControlledVocab
-    baseUrl="rs/hostLMSLocations"
-    canCreate={false}
-    columnMapping={{
-      supplyPreference: intl.formatMessage({ id: 'ui-rs.settings.lmsloc.supplyPreference' }),
-      correspondingDirectoryEntry: intl.formatMessage({ id: 'ui-rs.settings.lmsloc.correspondingDirectoryEntry' }),
-      actions: intl.formatMessage({ id: 'ui-rs.settings.actions' }),
-    }}
-    formType="final-form"
-    hiddenFields={['lastUpdated', 'numberOfObjects']}
-    readOnlyFields={['name']}
-    visibleFields={['name', 'supplyPreference', 'correspondingDirectoryEntry']}
-    label={intl.formatMessage({ id: 'ui-rs.settings.settingsSection.hostLMSLocations' })}
-    labelSingular={intl.formatMessage({ id: 'ui-rs.settings.lmsloc.hostLMSLocation' })}
-    limitParam="max"
-    objectLabel={<FormattedMessage id="ui-rs.settings.values" />}
-    sortby="name"
-    actionSuppressor={actionSuppressor}
-    fieldComponents={fieldComponents}
-    formatter={formatter}
-    stripes={stripes}
-  />);
+  return (
+    <Pane
+      defaultWidth="fill"
+      paneTitle={<FormattedMessage id="ui-rs.settings.settingsSection.hostLMSLocations" />}
+    >
+      <ActionList
+        actionAssigner={actionAssigner}
+        actionCalls={actionCalls}
+        columnMapping={{
+          name: <FormattedMessage id="ui-rs.settings.lmsloc.hostLMSLocation" />,
+          supplyPreference: <FormattedMessage id="ui-rs.settings.lmsloc.supplyPreference" />,
+          correspondingDirectoryEntry: <FormattedMessage id="ui-rs.settings.lmsloc.correspondingDirectoryEntry" />
+        }}
+        contentData={locations}
+        editableFields={{
+          name: () => false
+        }}
+        fieldComponents={fieldComponents}
+        formatter={{
+          correspondingDirectoryEntry: rec => {
+            const id = rec?.correspondingDirectoryEntry?.id;
+            if (!id) return '';
+            if (branchLocationsLoading) {
+              return <Spinner/>;
+            }
+            return dirLookup[id] ?? id;
+          }
+        }}
+        visibleFields={['name', 'supplyPreference', 'correspondingDirectoryEntry']}
+      />
+    </Pane>
+  );
 };
 
-HostLMSLocations.manifest = {
-  branchLocations: {
-    type: 'okapi',
-    path: 'rs/directoryEntry?filters=(type.value%3D%3Dbranch)&filters=status.value%3D%3Dmanaged&max=1000',
-  }
-};
-
-export default stripesConnect(HostLMSLocations);
+export default HostLMSLocations;
