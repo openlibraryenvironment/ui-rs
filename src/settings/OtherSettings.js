0af55f39dc3416ceb57527347429c447fb7d9896
Jason Skomorowski <jason@indexdata.com>
2022-05-19 18:00:02 -0400

    Avoid use of ControlledVocab for Other Settings PR-1264

--- src/settings/OtherSettings.js
+++ src/settings/OtherSettings.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { useIntl } from 'react-intl';
-import ControlledRefdata from './settingsComponents/ControlledRefdata';
-
-const actionSuppressor = {
-  edit: item => item.readOnly,
-  delete: () => true,
-};
+import { FormattedMessage, useIntl } from 'react-intl';
+import { EditableRefdataList } from '@k-int/stripes-kint-components';
+import { Pane } from '@folio/stripes/components';
+import { REFDATA_ENDPOINT } from '../constants/endpoints';
 
 const OtherSettings = () => {
   const intl = useIntl();
-  return (<ControlledRefdata
-    category="cancellationReasons"
-    label={intl.formatMessage({ id: 'ui-rs.settings.other.cancellationReasons' })}
-    formatter={{ label: r => intl.formatMessage({ id: `ui-rs.actions.requesterCancel.reasons.${r.value}`, defaultMessage: r.label }) }}
-    actionSuppressor={actionSuppressor}
-  />);
+  return (
+    <Pane paneTitle={<FormattedMessage id="ui-rs.settings.settingsSection.other" />}>
+      <EditableRefdataList
+        desc="cancellationReasons"
+        refdataEndpoint={REFDATA_ENDPOINT}
+        label={intl.formatMessage({ id: 'ui-rs.settings.other.cancellationReasons' })}
+      />
+    </Pane>
+  );
 };
 
 export default OtherSettings;
--- src/settings/settingsComponents/index.js
+++ src/settings/settingsComponents/index.js
@@ -1,2 +1,1 @@
 export { default as CustomISO18626 } from './CustomISO18626';
-export { default as ControlledRefdata } from './ControlledRefdata';
