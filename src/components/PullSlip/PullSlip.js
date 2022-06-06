ad2e6353c569a6a0e89520cd04b8617ddf5fb0c9
Jason Skomorowski <jason@indexdata.com>
2022-06-06 09:30:58 -0400

    Upgrade Stripes to Lotus PR-1306

--- src/components/PullSlip/PullSlip.js
+++ src/components/PullSlip/PullSlip.js
@@ -2,7 +2,9 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Parser } from 'html-to-react';
 import { injectIntl } from 'react-intl';
-import Handlebars from 'handlebars';
+// Need to directory dive here until we upgrade to something including this or similar:
+// https://github.com/handlebars-lang/handlebars.js/commit/c6c6bbb41f94b3ce7cbd68ede3fbc3f569dfc7e7
+import Handlebars from 'handlebars/lib/handlebars';
 import slipTemplate from './design/slip.handlebars';
 import wrapperTemplate from './design/singleslipwrapper.handlebars';
 import { establishStylesHook, recordToPullSlipData } from './util';
