ae4da2a093bfdbe7fed99f49fe50d44b732a3e40
Jason Skomorowski <jason@indexdata.com>
2022-12-22 13:48:15 -0500

    We now need to indicate that settings' translation are in ui-rs PR-1409
    ...part of the upgrade to stripes-kint-components 3.0+

--- src/settings/pullslipNotifications/PullslipNotifications.js
+++ src/settings/pullslipNotifications/PullslipNotifications.js
@@ -77,6 +77,7 @@ class PullslipNotifications extends React.Component {
         )}
       >
         <SettingPage
+          intlKey="ui-rs"
           sectionName="pullslipTemplateConfig"
         />
         <MultiColumnList
