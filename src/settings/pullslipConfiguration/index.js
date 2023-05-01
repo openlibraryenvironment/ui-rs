21b2165741974eda2afd55dedc3ef6f283c857e0
Jason Skomorowski <jason@indexdata.com>
2023-05-01 17:14:02 -0400

    Actually implement configurable pull slip report PR-1424

--- src/settings/pullslipConfiguration/index.js
+++ src/settings/pullslipConfiguration/index.js
@@ -2,6 +2,7 @@ import { Pane, Paneset } from '@folio/stripes/components';
 import { useOkapiQuery } from '@reshare/stripes-reshare';
 import { FormattedMessage } from 'react-intl';
 import FileSetting from './FileSetting';
+import ReportSetting from './ReportSetting';
 
 const PullslipConfiguration = () => {
   const { data: settings, isSuccess: settingsLoaded } = useOkapiQuery('rs/settings/appSettings', { searchParams: { filters: 'section==pullslipConfiguration' } });
@@ -13,7 +14,7 @@ const PullslipConfiguration = () => {
         paneTitle={<FormattedMessage id="ui-rs.settings.settingsSection.pullslipConfiguration" />}
       >
         <FileSetting setting={settings.filter(s => s.key === 'pull_slip_logo_id')[0]} fileType="LOGO" />
-        <FileSetting setting={settings.filter(s => s.key === 'pull_slip_report_id')[0]} fileType="REPORT_DEFINITION" />
+        <ReportSetting setting={settings.filter(s => s.key === 'pull_slip_report_id')[0]} fileType="REPORT_DEFINITION" />
       </Pane>
     </Paneset>
   );
