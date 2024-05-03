dc6c7527636946a97a8cc37f7f8f80884a8a6e7a
Jason Skomorowski <jason@indexdata.com>
2024-05-03 10:09:25 -0400

    Do not expose copyright type vocabulary for editing PR-1719

--- src/settings/OtherSettings.js
+++ src/settings/OtherSettings.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { Layout, Pane } from '@folio/stripes/components';
-import { useSettingSection } from '@k-int/stripes-kint-components';
+import { EditableRefdataList, useSettingSection } from '@k-int/stripes-kint-components';
 import { EditableSettingsList } from '@k-int/stripes-kint-components/es/lib/EditableSettingsList';
-import EditableRefdataList from '../components/EditableRefdataList';
 import { REFDATA_ENDPOINT, SETTINGS_ENDPOINT } from '../constants/endpoints';
 
 const OtherSettings = () => {
@@ -19,11 +18,6 @@ const OtherSettings = () => {
           desc="cancellationReasons"
           refdataEndpoint={REFDATA_ENDPOINT}
           label={intl.formatMessage({ id: 'ui-rs.settings.other.cancellationReasons' })}
-        />
-        <EditableRefdataList
-          desc="copyrightType"
-          refdataEndpoint={REFDATA_ENDPOINT}
-          label={intl.formatMessage({ id: 'ui-rs.settings.other.copyrightType' })}
         />
         <EditableSettingsList
           allowEdit
