e408254e227a566cc4aa8d7e6bc1db18dd48f02c
Jason Skomorowski <jason@indexdata.com>
2024-05-07 12:39:38 -0400

    Upgrade to poppy PR-1693

--- src/settings/noticePolicies/NoticeFieldArray.js
+++ src/settings/noticePolicies/NoticeFieldArray.js
@@ -1,34 +1,44 @@
-import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Field } from 'react-final-form';
+
+import { useKiwtFieldArray } from '@k-int/stripes-kint-components';
+
 import { Button } from '@folio/stripes/components';
-import { EditCard, withKiwtFieldArray } from '@folio/stripes-erm-components';
+import { EditCard } from '@folio/stripes-erm-components';
 
 import NoticeField from './NoticeField';
 
-const NoticeFieldArray = ({ items, name, options, onAddField, onDeleteField }) => (
-  <>
-    <div>
-      {items.map((notice, index) => (
-        <EditCard
-          key={index}
-          onDelete={() => onDeleteField(index, notice)}
-          header=""
-        >
-          <Field
-            component={NoticeField}
-            index={index}
-            name={`${name}[${index}]`}
-            options={options}
-            required
-          />
-        </EditCard>
-      ))}
-    </div>
-    <Button id="add-period-button" onClick={() => onAddField()}>
-      <FormattedMessage id="ui-rs.settings.noticePolicies.addNotice" />
-    </Button>
-  </>
-);
+const NoticeFieldArray = ({ fields: { name }, options }) => {
+  const {
+    items,
+    onAddField,
+    onDeleteField
+  } = useKiwtFieldArray(name);
 
-export default withKiwtFieldArray(NoticeFieldArray);
+  return (
+    <>
+      <div>
+        {items.map((notice, index) => (
+          <EditCard
+            key={index}
+            onDelete={() => onDeleteField(index, notice)}
+            header=""
+          >
+            <Field
+              component={NoticeField}
+              index={index}
+              name={`${name}[${index}]`}
+              options={options}
+              required
+            />
+          </EditCard>
+        ))}
+      </div>
+      <Button id="add-period-button" onClick={() => onAddField()}>
+        <FormattedMessage id="ui-rs.settings.noticePolicies.addNotice" />
+      </Button>
+    </>
+  );
+};
+
+export default NoticeFieldArray;
