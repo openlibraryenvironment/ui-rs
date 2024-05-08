8459106753b7b2cd1998837ea2a962174038e1e5
Edijs Timuls <edijs@indexdata.com>
2024-05-08 15:58:04 +0300

    PR-1727 - display a section in the UI with ZFL specific identifiers.

---
+++ src/components/ViewPatronRequest/sections/CustomIdentifiersInfo.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Card,
+  Col,
+  KeyValue,
+  Row,
+} from '@folio/stripes/components';
+
+import css from './CitationMetadata.css';
+
+class CitationMetadataInfo extends React.Component {
+  static propTypes = {
+    record: PropTypes.object,
+    id: PropTypes.string,
+  };
+
+  render() {
+    const { record } = this.props;
+    const { customIdentifiers } = record;
+    const summary = 'ZFL identifiers';
+
+    if (customIdentifiers && customIdentifiers.length > 0) {
+      return (
+          <Card
+              id={`${this.props.id}-card`}
+              headerStart={summary}
+              roundedBorder
+              cardClass={css.citationMetadataCard}
+              headerClass={css.citationMetadataCardHeader}
+          >
+            {customIdentifiers.map(id =>
+                <React.Fragment>
+                  <Row>
+                    <Col xs={6}>
+                      <KeyValue
+                          label={id.key}
+                          value={id.value}
+                      />
+                    </Col>
+                  </Row>
+                </React.Fragment>
+            )}
+          </Card>
+      );
+    } else {
+      return (<React.Fragment></React.Fragment>)
+    }
+  }
+}
+
+export default CitationMetadataInfo;
