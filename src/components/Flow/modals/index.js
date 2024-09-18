93494c3f4f9d8da130bf6e93468a1e33d8d02283
Edijs Timuls <edijs@indexdata.com>
2024-09-18 11:59:31 +0300

    PR-1944 - supplier: "abort supply" action should allow to specify a note and abort flag.

--- src/components/Flow/modals/index.js
+++ src/components/Flow/modals/index.js
@@ -4,6 +4,7 @@ import ActionReasonModal from '../ActionReasonModal';
 export const SupplierCannotSupply = props => <ActionReasonModal action="supplierCannotSupply" reasonVocab="cannotSupplyReasons" {...props} />;
 export const RequesterCancel = props => <ActionReasonModal action="requesterCancel" reasonVocab="cancellationReasons" {...props} />;
 export const CancelLocal = props => <ActionReasonModal action="cancelLocal" reasonVocab="cancellationReasons" {...props} />;
+export const SlnpAbortSupply = props => <ActionReasonModal action="slnpAbortSupply" reasonVocab="slnpCancelOrAbortSupplyReasons" {...props} />;
 
 export const Cancel = () => null;
 export { default as FillMultiVolumeRequest } from './FillMultiVolumeRequest';
